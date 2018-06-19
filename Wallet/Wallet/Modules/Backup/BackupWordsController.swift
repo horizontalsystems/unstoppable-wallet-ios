5c652aceaffb6c60a32a090b264be73f0fdd7505
max <trickster77777@gmail.com>
2018-06-19 15:38:54 +0600

    Make words controller follow specs.

--- Wallet/Wallet/Modules/Backup/BackupWordsController.swift
+++ Wallet/Wallet/Modules/Backup/BackupWordsController.swift
@@ -4,7 +4,6 @@ class BackupWordsController: UIViewController {
 
     let delegate: IBackupViewDelegate
 
-    @IBOutlet weak var descriptionLabel: UILabel?
     @IBOutlet weak var wordsLabel: UILabel?
     @IBOutlet weak var backButton: UIButton?
     @IBOutlet weak var proceedButton: UIButton?
@@ -25,11 +24,17 @@ class BackupWordsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        descriptionLabel?.text = "backup.words.description".localized
+        title = "backup.words.title".localized
+
         backButton?.setTitle("backup.words.back".localized, for: .normal)
         proceedButton?.setTitle("backup.words.proceed".localized, for: .normal)
 
-        wordsLabel?.text = words.enumerated().map { "\($0 + 1). \($1)" }.joined(separator: "\n")
+        let joinedWords = words.enumerated().map { "\($0 + 1). \($1)" }.joined(separator: "\n")
+        let attributedText = wordsLabel?.attributedText as? NSMutableAttributedString
+        attributedText?.mutableString.setString(joinedWords)
+        attributedText?.addAttribute(NSAttributedStringKey.font, value: UIFont.systemFont(ofSize: 22, weight: .bold), range: NSMakeRange(0, joinedWords.count))
+        wordsLabel?.attributedText = attributedText
+
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
