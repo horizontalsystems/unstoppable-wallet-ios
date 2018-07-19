a23280ebef9a51be07dd457900fd6501f33bcd0d
Ermat <ealymbaev@gmail.com>
2018-07-19 11:26:34 +0600

    Implement BlockHeaderItemSaver

--- WalletKit/WalletKit/Managers/BlockHeaderItemSaver.swift
+++ WalletKit/WalletKit/Managers/BlockHeaderItemSaver.swift
@@ -3,8 +3,33 @@ import Foundation
 class BlockHeaderItemSaver {
     static let shared = BlockHeaderItemSaver()
 
+    let realmFactory: RealmFactory
+
+    init(realmFactory: RealmFactory = .shared) {
+        self.realmFactory = realmFactory
+    }
+
     func save(lastHeight: Int, items: [BlockHeaderItem]) {
-        // TODO: to be implemented
+        let realm = realmFactory.realm
+
+        var currentHeight = lastHeight
+        var blocks = [Block]()
+
+        for item in items {
+            currentHeight += 1
+
+            let hash = Crypto.sha256sha256(item.serialized())
+
+            let block = Block()
+            block.reversedHeaderHashHex = hash.reversedHex
+            block.height = currentHeight
+
+            blocks.append(block)
+        }
+
+        try? realm.write {
+            realm.add(blocks, update: true)
+        }
     }
 
 }
