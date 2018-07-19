213e365f26dc08104e92c6510c1d105c4b0ab67e
Ermat <ealymbaev@gmail.com>
2018-07-19 11:07:24 +0600

    Implement BlockHeaderItemValidator: check the validity of previous block hashes

--- WalletKit/WalletKit/Managers/BlockHeaderItemValidator.swift
+++ WalletKit/WalletKit/Managers/BlockHeaderItemValidator.swift
@@ -4,8 +4,19 @@ class BlockHeaderItemValidator {
     static let shared = BlockHeaderItemValidator()
 
     func filterValidItems(initialHash: Data, items: [BlockHeaderItem]) -> [BlockHeaderItem] {
-        // TODO: to be implemented
-        return []
+        var validItems = [BlockHeaderItem]()
+
+        for item in items {
+            let previousHash = validItems.last.map { Crypto.sha256sha256($0.serialized()) } ?? initialHash
+
+            if previousHash == item.prevBlock {
+                validItems.append(item)
+            } else {
+                break
+            }
+        }
+
+        return validItems
     }
 
 }
