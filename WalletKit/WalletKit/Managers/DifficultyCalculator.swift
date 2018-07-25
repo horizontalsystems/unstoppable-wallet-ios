bb278d2211a70500f9e8722b2e6d1d0652a61c30
Anton Stavnichiy <md.belichenko@gmail.com>
2018-07-25 11:24:13 +0600

    Add testnet headers validation.

--- WalletKit/WalletKit/Managers/DifficultyCalculator.swift
+++ WalletKit/WalletKit/Managers/DifficultyCalculator.swift
@@ -4,10 +4,11 @@ import BigInt
 class DifficultyCalculator {
     static let shared = DifficultyCalculator()
 
-    private let maxTargetDifficulty: UInt32 = 0x1d00ffff             // Maximum difficulty.
+    private let maxTargetBits: UInt32 = 0x1d00ffff                   // Maximum difficulty.
+    var maxTargetDifficulty: BigInt { return difficultyEncoder.decodeCompact(bits: maxTargetBits) }
 
-    private let targetTimeSpan: Int = 14 * 24 * 60 * 60              // 2 weeks per difficulty cycle, on average.
-    private let targetSpacing: Int = 10 * 60                         // 10 minutes per block.
+    private let targetTimeSpan: Int = 14 * 24 * 60 * 60      // 2 weeks per difficulty cycle, on average.
+    let targetSpacing: Int = 10 * 60                         // 10 minutes per block.
     let heightInterval: Int
 
     let difficultyEncoder: DifficultyEncoder
@@ -21,14 +22,14 @@ class DifficultyCalculator {
         return min(max(timeSpan, targetTimeSpan / 4), targetTimeSpan * 4)
     }
 
-    public func difficultyAfter(item: BlockHeaderItem, checkPointBlock: Block, height: Int) -> UInt32 {
+    func difficultyAfter(item: BlockHeaderItem, checkPointBlock: Block, height: Int) -> UInt32 {
         let checkPointItem = BlockHeaderItem.deserialize(byteStream: ByteStream(checkPointBlock.rawHeader))
         let timeSpan = limit(timeSpan: Int(item.timestamp - checkPointItem.timestamp))
 
         var bigIntDifficulty = difficultyEncoder.decodeCompact(bits: item.bits)
         bigIntDifficulty *= BigInt(timeSpan)
         bigIntDifficulty /= BigInt(targetTimeSpan)
-        let newDifficulty = min(difficultyEncoder.encodeCompact(from: bigIntDifficulty), maxTargetDifficulty)
+        let newDifficulty = min(difficultyEncoder.encodeCompact(from: bigIntDifficulty), maxTargetBits)
 
         return UInt32(newDifficulty)
     }
