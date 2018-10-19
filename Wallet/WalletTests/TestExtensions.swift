6670834cc9667a298b2be6ed9f0d8b838eda4277
Ermat <ealymbaev@gmail.com>
2018-10-19 18:22:41 +0600

    Fix Test Extensions

--- Wallet/WalletTests/TestExtensions.swift
+++ Wallet/WalletTests/TestExtensions.swift
@@ -1,30 +1,20 @@
 import Foundation
 @testable import Bank
 
-extension Coin: Equatable {
-    public static func ==(lhs: Coin, rhs: Coin) -> Bool {
-        return lhs.name == rhs.name &&
-                lhs.code == rhs.code
-    }
-}
-
 extension CoinValue: Equatable {
     public static func ==(lhs: CoinValue, rhs: CoinValue) -> Bool {
-        return lhs.coin == rhs.coin &&
-                lhs.value == rhs.value
+        return lhs.coin == rhs.coin && lhs.value == rhs.value
     }
 }
 
 extension Currency: Equatable {
     public static func ==(lhs: Currency, rhs: Currency) -> Bool {
-        return lhs.code == rhs.code &&
-                lhs.symbol == rhs.symbol
+        return lhs.code == rhs.code && lhs.symbol == rhs.symbol
     }
 }
 
 extension CurrencyValue: Equatable {
     public static func ==(lhs: CurrencyValue, rhs: CurrencyValue) -> Bool {
-        return lhs.currency == rhs.currency &&
-                lhs.value == rhs.value
+        return lhs.currency == rhs.currency && lhs.value == rhs.value
     }
 }
