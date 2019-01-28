cf693f8d9f01e6f2c13270268171cb36de8aadb0
Ermat <ealymbaev@gmail.com>
2019-01-28 10:51:50 +0600

    Apply new design for transaction filters (#280)

--- BankWallet/BankWallet/Theme/TransactionsFilterTheme.swift
+++ BankWallet/BankWallet/Theme/TransactionsFilterTheme.swift
@@ -2,20 +2,19 @@ import UIKit
 
 class TransactionsFilterTheme {
     static let cornerRadius: CGFloat = 14
-    static let borderColor = UIColor.cryptoYellow
-    static let borderWidth:CGFloat = 1
-    static let nameFont = UIFont.cryptoCaption2
+    static let borderColor: UIColor = .cryptoSteel20
+    static let borderWidth: CGFloat = 1
+    static let nameFont: UIFont = .systemFont(ofSize: 13)
     static let nameVerticalMargin: CGFloat = 8
-    static let nameHorizontalMargin: CGFloat = 24
+    static let nameHorizontalMargin: CGFloat = 32
     static let nameMinWidth: CGFloat = 87
 
     static let spacing: CGFloat = 8
 
-    static let deselectedNameColor = UIColor.cryptoYellow
-    static let selectedNameColor = UIColor.black
-    static let deselectedBackgroundColor = UIColor.clear
-    static let selectedBackgroundColor = UIColor.cryptoYellow
+    static let selectedNameColor: UIColor = .cryptoDark
+    static var deselectedNameColor: UIColor { return .crypto_Bars_Dark }
+    static let selectedBackgroundColor: UIColor = .cryptoYellow
+    static let deselectedBackgroundColor: UIColor = .cryptoSteel20
 
     static let filterHeaderHeight: CGFloat = 44
 }
-                        
