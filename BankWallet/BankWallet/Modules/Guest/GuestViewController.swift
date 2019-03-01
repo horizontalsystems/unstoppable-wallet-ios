5a8f167cbe5e2506e193901b52929f3b2ddea9f9
max <trickster77777@gmail.com>
2019-03-01 16:09:13 +0600

    Remove xib from guest controller (#510)

--- BankWallet/BankWallet/Modules/Guest/GuestViewController.swift
+++ BankWallet/BankWallet/Modules/Guest/GuestViewController.swift
@@ -1,17 +1,19 @@
 import UIKit
+import SnapKit
 
 class GuestViewController: UIViewController {
     private let delegate: IGuestViewDelegate
 
-    @IBOutlet weak var titleLabel: UILabel?
-    @IBOutlet weak var createButton: UIButton?
-    @IBOutlet weak var importButton: UIButton?
-    @IBOutlet weak var versionLabel: UILabel?
+    var backgroundImageView = UIImageView()
+    var titleLabel = UILabel()
+    var createButton = UIButton()
+    var importButton = UIButton()
+    var versionLabel = UILabel()
 
     init(delegate: IGuestViewDelegate) {
         self.delegate = delegate
 
-        super.init(nibName: String(describing: GuestViewController.self), bundle: nil)
+        super.init(nibName: nil, bundle: nil)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -20,22 +22,73 @@ class GuestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(backgroundImageView)
+        backgroundImageView.snp.makeConstraints { maker in
+            maker.edges.equalToSuperview()
+        }
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.image = UIImage(named: "Blockchain Image")
+
+        view.addSubview(versionLabel)
+        versionLabel.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(-GuestTheme.versionBottomMargin)
+        }
+        versionLabel.textColor = GuestTheme.versionLabelTextColor
+        versionLabel.font = GuestTheme.versionLabelFont
+
+        view.addSubview(importButton)
+        importButton.titleLabel?.font = GuestTheme.buttonFont
+        importButton.setBackgroundColor(color: GuestTheme.importButtonBackground, forState: .normal)
+        importButton.setTitleColor(GuestTheme.importButtonTextColor, for: .normal)
+        importButton.layer.cornerRadius = GuestTheme.buttonCornerRadius
+        importButton.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.bottom.equalTo(self.versionLabel.snp.top).offset(-GuestTheme.importButtonBottomMargin)
+            maker.height.equalTo(GuestTheme.buttonHeight)
+            maker.leading.equalToSuperview().offset(GuestTheme.buttonSideMargin)
+            maker.trailing.equalToSuperview().offset(-GuestTheme.buttonSideMargin)
+        }
+        importButton.addTarget(self, action: #selector(restoreWalletDidTap), for: .touchUpInside)
+
+        view.addSubview(createButton)
+        createButton.titleLabel?.font = GuestTheme.buttonFont
+        createButton.setBackgroundColor(color: GuestTheme.createButtonBackground, forState: .normal)
+        createButton.setTitleColor(GuestTheme.createButtonTextColor, for: .normal)
+        createButton.layer.cornerRadius = GuestTheme.buttonCornerRadius
+        createButton.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.bottom.equalTo(self.importButton.snp.top).offset(-GuestTheme.createButtonBottomMargin)
+            maker.height.equalTo(GuestTheme.buttonHeight)
+            maker.leading.equalToSuperview().offset(GuestTheme.buttonSideMargin)
+            maker.trailing.equalToSuperview().offset(-GuestTheme.buttonSideMargin)
+        }
+        createButton.addTarget(self, action: #selector(createNewWalletDidTap), for: .touchUpInside)
+
+        view.addSubview(titleLabel)
+        titleLabel.font = GuestTheme.titleFont
+        titleLabel.textColor = GuestTheme.titleColor
+        titleLabel.snp.makeConstraints { maker in
+            maker.centerX.equalToSuperview()
+            maker.bottom.equalTo(self.createButton.snp.top).offset(-GuestTheme.titleBottomMargin)
+        }
+
         delegate.viewDidLoad()
 
-        titleLabel?.text = "guest.title".localized
-        createButton?.setTitle("guest.create_wallet".localized, for: .normal)
-        importButton?.setTitle("guest.restore_wallet".localized, for: .normal)
+        titleLabel.text = "guest.title".localized
+        createButton.setTitle("guest.create_wallet".localized, for: .normal)
+        importButton.setTitle("guest.restore_wallet".localized, for: .normal)
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return AppTheme.statusBarStyle
     }
 
-    @IBAction func createNewWalletDidTap() {
+    @objc func createNewWalletDidTap() {
         delegate.createWalletDidClick()
     }
 
-    @IBAction func restoreWalletDidTap() {
+    @objc func restoreWalletDidTap() {
         delegate.restoreWalletDidClick()
     }
 
@@ -44,7 +97,7 @@ class GuestViewController: UIViewController {
 extension GuestViewController: IGuestView {
 
     func set(appVersion: String) {
-        versionLabel?.text = appVersion
+        versionLabel.text = appVersion
     }
 
 }
---
+++ BankWallet/BankWallet/Theme/GuestTheme.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class GuestTheme {
+    static let titleFont: UIFont = .cryptoTitleHeavy
+    static let titleColor: UIColor = .white
+    static let titleBottomMargin: CGFloat = 24
+    static let createButtonTextColor: UIColor = .black
+    static let createButtonBottomMargin: CGFloat = 16
+    static let createButtonBackground: UIColor = .cryptoYellow
+    static let importButtonTextColor: UIColor = .white
+    static let importButtonBottomMargin: CGFloat = 12
+    static let importButtonBackground: UIColor = .cryptoSteel20
+    static let versionLabelTextColor: UIColor = .cryptoGray
+    static let versionBottomMargin: CGFloat = 11
+    static let versionLabelFont: UIFont = .cryptoCaption2Medium
+    static let buttonFont: UIFont = .cryptoHeadline
+    static let buttonHeight: CGFloat = 50
+    static let buttonSideMargin: CGFloat = 44
+    static let buttonCornerRadius: CGFloat = 8
+}
