d5912a5f2b7447d5281466cd40a5b9eb78cdcf5b
max <trickster77777@gmail.com>
2019-04-05 14:49:28 +0600

    Update about controller (#532)

--- BankWallet/BankWallet/Modules/Settings/About/AboutCell.swift
+++ BankWallet/BankWallet/Modules/Settings/About/AboutCell.swift
@@ -6,10 +6,12 @@ class AboutCell: UITableViewCell {
     @IBOutlet weak var subtitleLabel: UILabel?
     @IBOutlet weak var termsHeaderLabel: UILabel?
     @IBOutlet weak var termsLabel: UILabel?
+    @IBOutlet weak var separatorView: UIView?
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
+        separatorView?.backgroundColor = .cryptoSteel20
         titleLabel?.text = "guest.title".localized
         titleLabel?.textColor = .crypto_White_Black
         subtitleLabel?.text = "guest.subtitle".localized
