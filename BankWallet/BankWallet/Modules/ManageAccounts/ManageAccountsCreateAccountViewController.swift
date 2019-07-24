e1ab2d0fdde25bb9fb6f689fb64682edefaad670
Ermat <ealymbaev@gmail.com>
2019-07-24 15:47:40 +0600

    Manage Accounts module: create confirmation (#768)

---
+++ BankWallet/BankWallet/Modules/ManageAccounts/ManageAccountsCreateAccountViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+import ActionSheet
+
+class ManageAccountsCreateAccountViewController: ActionSheetController {
+    private let predefinedAccountType: IPredefinedAccountType
+    private let titleItem: AlertTitleItem
+    private let textItem: AlertTextItem
+
+    init(predefinedAccountType: IPredefinedAccountType, onCreate: @escaping () -> ()) {
+        self.predefinedAccountType = predefinedAccountType
+
+        titleItem = AlertTitleItem(
+                title: predefinedAccountType.title.localized,
+                icon: UIImage(named: "Key Icon")?.withRenderingMode(.alwaysTemplate),
+                iconTintColor: ManageAccountsTheme.alertKeyImageColor,
+                tag: 0
+        )
+
+        textItem = AlertTextItem(text: "settings_manage_keys.create.text".localized(predefinedAccountType.coinCodes.localized), tag: 1)
+
+        super.init(withModel: BaseAlertModel(), actionSheetThemeConfig: AppTheme.actionSheetConfig)
+
+        model.addItemView(titleItem)
+        model.addItemView(textItem)
+
+        let createItem = AlertButtonItem(
+                tag: 2,
+                title: "settings_manage_keys.create".localized,
+                textStyle: ButtonTheme.textColorDictionary,
+                backgroundStyle: ButtonTheme.yellowBackgroundDictionary,
+                insets: UIEdgeInsets(top: ButtonTheme.verticalMargin, left: ButtonTheme.margin, bottom: ButtonTheme.verticalMargin, right: ButtonTheme.margin)
+        ) { [weak self] in
+            self?.dismiss(animated: true) {
+                onCreate()
+            }
+        }
+
+        createItem.isActive = true
+        model.addItemView(createItem)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        backgroundColor = AppTheme.actionSheetBackgroundColor
+        contentBackgroundColor = .white
+    }
+
+}
