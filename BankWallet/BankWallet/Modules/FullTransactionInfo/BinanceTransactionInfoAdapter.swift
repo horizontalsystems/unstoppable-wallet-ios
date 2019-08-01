0adf6ca5bae38b0535b787698b11533defcef6b1
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2019-08-01 16:53:37 +0600

    Binance.org full transaction info provider integrated (#815)

---
+++ BankWallet/BankWallet/Modules/FullTransactionInfo/BinanceTransactionInfoAdapter.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+class BinanceTransactionInfoAdapter: IFullTransactionInfoAdapter {
+    private let provider: IBinanceProvider
+    private let coin: Coin
+
+    init(provider: IBinanceProvider, coin: Coin) {
+        self.provider = provider
+        self.coin = coin
+    }
+
+    func convert(json: [String: Any]) -> FullTransactionRecord? {
+        guard let txResponse = provider.convert(json: json) else {
+            return nil
+        }
+
+        var sections = [FullTransactionSection]()
+
+        // Top Section
+
+        var topSectionItems = [FullTransactionItem]()
+        if let blockHeight = txResponse.blockHeight {
+            topSectionItems.append(FullTransactionItem(icon: "Block Icon", title: "full_info.block".localized, value: "#\(blockHeight)"))
+        }
+        if let centValue = txResponse.value {
+            let value = centValue / pow(10, 8)
+            let coinValue = CoinValue(coinCode: coin.code, value: value)
+            topSectionItems.append(FullTransactionItem(title: "full_info.amount".localized, value: ValueFormatter.instance.format(coinValue: coinValue)))
+        }
+        if !topSectionItems.isEmpty {
+            sections.append(FullTransactionSection(title: nil, items: topSectionItems))
+        }
+
+        // Fee
+
+        var feeGasItems = [FullTransactionItem]()
+        if let centValue = txResponse.fee {
+            let fee = centValue / pow(10, 8)
+            let feeValue = CoinValue(coinCode: "BNB", value: fee)
+            feeGasItems.append(FullTransactionItem(title: "full_info.fee".localized, value: ValueFormatter.instance.format(coinValue: feeValue)))
+        }
+        if !feeGasItems.isEmpty {
+            sections.append(FullTransactionSection(title: nil, items: feeGasItems))
+        }
+
+        // From / To
+
+        var inputOutputItems = [FullTransactionItem]()
+        if let from = txResponse.from {
+            inputOutputItems.append(FullTransactionItem(title: "full_info.from".localized, value: from, clickable: true, showExtra: .icon))
+        }
+        if let to = txResponse.to {
+            inputOutputItems.append(FullTransactionItem(title: "full_info.to".localized, value: to, clickable: true, showExtra: .icon))
+        }
+        if !inputOutputItems.isEmpty {
+            sections.append(FullTransactionSection(title: nil, items: inputOutputItems))
+        }
+
+        return FullTransactionRecord(providerName: provider.name, sections: sections)
+    }
+}
