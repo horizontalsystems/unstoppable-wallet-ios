66ce91a61f871ef3c82f21ddd257e864d09797c3
diana <imanahunovad@gmail.com>
2019-08-20 10:21:15 +0600

    Change button Forward to Share in receive controller

--- BankWallet/BankWallet/Modules/Deposit/ViewItems/DepositShareButtonItem.swift
+++ BankWallet/BankWallet/Modules/Deposit/ViewItems/DepositShareButtonItem.swift
@@ -4,7 +4,7 @@ import ActionSheet
 class DepositShareButtonItem: BaseButtonItem {
     override var backgroundStyle: RespondButton.Style { return ButtonTheme.greenBackgroundDictionary }
     override var textStyle: RespondButton.Style { return ButtonTheme.textColorDictionary }
-    override var title: String { return "button.forward".localized }
+    override var title: String { return "button.share".localized }
 
     init(tag: Int, onTap: @escaping (() -> ())) {
         super.init(cellType: DepositShareButtonItemView.self, tag: tag, required: true)
