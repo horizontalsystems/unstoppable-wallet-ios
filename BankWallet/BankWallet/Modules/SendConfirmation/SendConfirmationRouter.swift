8df17f6c599b2490756066facf354607db61c864
Anton Stavnichiy <md.belichenko@gmail.com>
2019-08-28 17:23:18 +0600

    Implement Send Confirmation module
    - call delegate.send when tap button
    - remove commented code

--- BankWallet/BankWallet/Modules/SendConfirmation/SendConfirmationRouter.swift
+++ BankWallet/BankWallet/Modules/SendConfirmation/SendConfirmationRouter.swift
@@ -4,35 +4,6 @@ import SnapKit
 class SendConfirmationRouter {
 
     static func module(viewItems: [ISendConfirmationViewItemNew], delegate: ISendConfirmationDelegate) -> UIViewController {
-//        var views = [UIView]()
-//
-//        let primaryView = SendConfirmationPrimaryRouter.module(primaryAmount: item.primaryAmount, secondaryAmount: item.secondaryAmount, receiver: item.receiver)
-//        views.append(primaryView)
-//
-//        var memoModule: ISendConfirmationMemoModule?
-//
-//        if item.showMemo {
-//            let (memoView, memo) = SendConfirmationMemoRouter.module()
-//            memoModule = memo
-//            views.append(memoView)
-//        }
-//
-//        var textFields = [UIView]()
-//        if let feeInfo = item.feeInfo {
-//            textFields.append(SendConfirmationFieldView(title: "send.fee".localized, text: feeInfo))
-//        }
-//        if let totalInfo = item.totalInfo {
-//            textFields.append(SendConfirmationFieldView(title: "send.confirmation.total".localized, text: totalInfo))
-//        }
-//        if let estimateTime = item.estimateTime {
-//            textFields.append(SendConfirmationFieldView(title: "send.confirmation.estimate_time".localized, text: estimateTime))
-//        }
-//        if !textFields.isEmpty {
-//            let fieldSectionSeparatorView = SendConfirmationSeparatorView(height: SendTheme.confirmationFieldSectionTopMargin)
-//            views.append(fieldSectionSeparatorView)
-//
-//            views.append(contentsOf: textFields)
-//        }
         let interactor = SendConfirmationInteractor(pasteboardManager: App.shared.pasteboardManager)
         let presenter = SendConfirmationPresenter(interactor: interactor, viewItems: viewItems)
         let viewController = SendConfirmationViewController(delegate: presenter)
