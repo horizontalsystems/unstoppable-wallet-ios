0f73fbe31d5c14042e32301d0437f304d81da49f
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2019-09-03 10:58:15 +0600

    Extract EosAction to separate file

---
+++ BankWallet/BankWallet/Modules/FullTransactionInfo/Models/EosAction.swift
@@ -0,0 +1,7 @@
+struct EosAction {
+    let contract: String?
+    let from: String?
+    let to: String?
+    let quantity: String?
+    let memo: String?
+}
--- BankWallet/BankWallet/Modules/FullTransactionInfo/TransactionProviders/EosInfraProvider.swift
+++ BankWallet/BankWallet/Modules/FullTransactionInfo/TransactionProviders/EosInfraProvider.swift
@@ -119,11 +119,3 @@ extension EosResponse {
     }
 
 }
-
-struct EosAction {
-    let contract: String?
-    let from: String?
-    let to: String?
-    let quantity: String?
-    let memo: String?
-}
