97c8290532a1883f4ae12b0b639b775f40ee3df7
Anton Stavnichiy <md.belichenko@gmail.com>
2019-09-30 13:20:10 +0600

    Fix after merging

--- BankWallet/BankWallet/UserInterface/Controllers/WalletNavigationController.swift
+++ BankWallet/BankWallet/UserInterface/Controllers/WalletNavigationController.swift
@@ -31,7 +31,6 @@ class WalletNavigationController: UINavigationController {
         }
     }
 
-        modalPresentationStyle = .fullScreen
     open override var childForStatusBarStyle: UIViewController? {
         self.topViewController
     }
