0ebec2b0ccef19028791991fdd98802c670db44b
Ermat <ealymbaev@gmail.com>
2019-12-13 11:47:53 +0600

    Add LaunchManager for handling first launch in background thread

---
+++ BankWallet/BankWallet/Core/Managers/LaunchManager.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+class LaunchManager {
+    private let localStorage: ILocalStorage
+    private let secureStorage: ISecureStorage
+
+    init(localStorage: ILocalStorage, secureStorage: ISecureStorage) {
+        self.localStorage = localStorage
+        self.secureStorage = secureStorage
+    }
+
+    private func handleDidLaunchOnce() {
+        if !localStorage.didLaunchOnce {
+            try? secureStorage.clear()
+            localStorage.didLaunchOnce = true
+        }
+    }
+
+}
+
+extension LaunchManager: ILaunchManager {
+
+    func handleFirstLaunch() {
+        DispatchQueue.global(qos: .background).async {
+            self.handleDidLaunchOnce()
+        }
+    }
+
+}
+
+protocol ILaunchManager {
+    func handleFirstLaunch()
+}
