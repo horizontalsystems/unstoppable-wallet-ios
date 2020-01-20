282906216c46e1d5e508a9f10d85e90ae0164050
anton <ant013@mail.ru>
2020-01-20 10:16:00 +0600

    Show addressType in deposit view for bitcoin.

--- BankWallet/BankWallet/Modules/Deposit/ViewItems/CollectionView/AddressItem.swift
+++ BankWallet/BankWallet/Modules/Deposit/ViewItems/CollectionView/AddressItem.swift
@@ -1,12 +1,13 @@
 struct AddressItem {
     let coin: Coin
+    let addressType: String?
     let address: String
 }
 
 extension AddressItem: Equatable {
 
     public static func ==(lhs: AddressItem, rhs: AddressItem) -> Bool {
-        return lhs.coin == rhs.coin && lhs.address == rhs.address
+        lhs.coin == rhs.coin && lhs.address == rhs.address
     }
 
 }
