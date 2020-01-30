bde8a63253e01de20d834b6cf5c1fb21a65b2c8b
Ermat <ealymbaev@gmail.com>
2020-01-30 18:57:02 +0600

    Integrate LanguageKit
    - remove extracted localizations

--- BankWallet/BankWallet/Core/Managers/LanguageManager.swift
+++ BankWallet/BankWallet/Core/Managers/LanguageManager.swift
@@ -3,11 +3,11 @@ import LanguageKit
 extension String {
 
     var localized: String {
-        LanguageManager.shared.localize(string: self)
+        LanguageManager.shared.localize(string: self, bundle: Bundle.main)
     }
 
     func localized(_ arguments: CVarArg...) -> String {
-        LanguageManager.shared.localize(string: self, arguments: arguments)
+        LanguageManager.shared.localize(string: self, bundle: Bundle.main, arguments: arguments)
     }
 
 }
