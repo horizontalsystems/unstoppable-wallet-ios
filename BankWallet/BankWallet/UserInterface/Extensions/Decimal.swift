32b8e2edbab8c39c401195459cfa62de6e582c9d
Anton <anton.stavnichiy@gmail.com>
2020-01-30 17:32:54 +0600

    Rounding logic for current exchange rate for the coin

--- BankWallet/BankWallet/UserInterface/Extensions/Decimal.swift
+++ BankWallet/BankWallet/UserInterface/Extensions/Decimal.swift
@@ -1,7 +1,20 @@
 import Foundation
 
 extension Decimal {
+
     var decimalCount: Int {
-        return max(-exponent, 0)
+        max(-exponent, 0)
     }
+
+    func significantDecimalCount(threshold: Int, maxDecimals: Int) -> Int {
+        let thresholdRange = pow(10, threshold - 1)
+
+        for decimalCount in 0..<maxDecimals {
+            if self * pow(10, decimalCount) >= thresholdRange {
+                return decimalCount
+            }
+        }
+        return maxDecimals
+    }
+
 }
