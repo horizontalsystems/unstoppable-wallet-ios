2d7ede334497d6c1b9486cdca1236d8821e26e9a
anton <ant013@mail.ru>
2020-02-17 14:55:21 +0600

    Fix lock screen on enter foreground.

--- BankWallet/BankWallet/UserInterface/PinKitDelegate.swift
+++ BankWallet/BankWallet/UserInterface/PinKitDelegate.swift
@@ -14,7 +14,7 @@ extension PinKitDelegate: IPinKitDelegate {
             controller = presentedController
         }
 
-        controller?.present(App.shared.pinKit.unlockPinModule(delegate: delegate, enableBiometry: true, presentationStyle: .simple, cancellable: false), animated: true)
+        controller?.present(LockScreenRouter.module(pinKit: App.shared.pinKit, appStart: false), animated: true)
     }
 
 }
