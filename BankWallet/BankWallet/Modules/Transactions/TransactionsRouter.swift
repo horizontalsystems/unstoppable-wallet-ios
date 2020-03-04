3c50f6dff9e02ecc55385c5d1eb45028fd51d030
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2020-03-04 17:42:07 +0600

    Merge TransactionsLoader to TransactionsPresenter

--- BankWallet/BankWallet/Modules/Transactions/TransactionsRouter.swift
+++ BankWallet/BankWallet/Modules/Transactions/TransactionsRouter.swift
@@ -16,15 +16,13 @@ extension TransactionsRouter {
 
     static func module() -> UIViewController {
         let dataSource = TransactionRecordDataSource(poolRepo: TransactionRecordPoolRepo(), itemsDataSource: TransactionItemDataSource(), factory: TransactionItemFactory())
-        let loader = TransactionsLoader(dataSource: dataSource)
         let transactionViewItemLoader = TransactionViewItemLoader(state: TransactionViewItemLoaderState(), differ: Differ())
 
         let router = TransactionsRouter()
         let interactor = TransactionsInteractor(walletManager: App.shared.walletManager, adapterManager: App.shared.adapterManager, currencyKit: App.shared.currencyKit, rateManager: App.shared.rateManager, reachabilityManager: App.shared.reachabilityManager)
-        let presenter = TransactionsPresenter(interactor: interactor, router: router, factory: TransactionViewItemFactory(feeCoinProvider: App.shared.feeCoinProvider), loader: loader, dataSource: TransactionsMetadataDataSource(), viewItemLoader: transactionViewItemLoader)
+        let presenter = TransactionsPresenter(interactor: interactor, router: router, factory: TransactionViewItemFactory(feeCoinProvider: App.shared.feeCoinProvider), dataSource: dataSource, metaDataSource: TransactionsMetadataDataSource(), viewItemLoader: transactionViewItemLoader)
         let viewController = TransactionsViewController(delegate: presenter)
 
-        loader.delegate = presenter
         interactor.delegate = presenter
         presenter.view = viewController
         router.viewController = viewController
