7ba5f43423de542e7c250426db308081420fdbbb
Ermat <ealymbaev@gmail.com>
2020-03-18 16:26:07 +0600

    Change Litecoin api url

--- BankWallet/BankWallet/Models/CoinType.swift
+++ BankWallet/BankWallet/Models/CoinType.swift
@@ -68,7 +68,7 @@ enum CoinType {
     var restoreUrl: String {
         switch self {
         case .bitcoin: return "https://btc.horizontalsystems.xyz/apg"
-        case .litecoin: return "https://ltc.horizontalsystems.xyz/apg"
+        case .litecoin: return "https://ltc.horizontalsystems.xyz/api"
         case .bitcoinCash: return "https://explorer.bitcoin.com/bch/"
         case .dash: return "https://dash.horizontalsystems.xyz"
         default: return ""
