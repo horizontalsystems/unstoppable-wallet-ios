05eab9b64b91c416dea92d6462ed6f3eef197f04
max <trickster77777@gmail.com>
2020-04-20 11:58:19 +0600

    Update and implement new PinKit
    - disable auto biometry

--- BankWallet/BankWallet/Modules/LockScreen/LockScreenRouter.swift
+++ BankWallet/BankWallet/Modules/LockScreen/LockScreenRouter.swift
@@ -35,8 +35,9 @@ extension LockScreenRouter {
         let router = LockScreenRouter(appStart: appStart)
         let presenter = LockScreenPresenter(router: router)
 
+        let insets = UIEdgeInsets(top: LockScreenController.pageControlHeight, left: 0, bottom: 60, right: 0)
         let rateListController = RateListRouter.module(delegate: presenter, topMargin: LockScreenController.pageControlHeight)
-        let unlockController = pinKit.unlockPinModule(delegate: presenter, enableBiometry: true, presentationStyle: .simple, cancellable: false)
+        let unlockController = pinKit.unlockPinModule(delegate: presenter, biometryUnlockMode: .enabled, insets: insets, cancellable: false)
 
         let viewController = LockScreenController(viewControllers: [unlockController, rateListController])
         router.viewController = viewController
