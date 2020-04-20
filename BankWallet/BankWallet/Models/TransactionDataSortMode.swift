a276d5569bef68c0c884152c186b19f2bbc822a1
max <trickster77777@gmail.com>
2020-04-20 14:27:00 +0600

    Remove default sort mode

--- BankWallet/BankWallet/Models/TransactionDataSortMode.swift
+++ BankWallet/BankWallet/Models/TransactionDataSortMode.swift
@@ -1,11 +1,9 @@
 enum TransactionDataSortMode: String, CaseIterable {
-    case none
     case shuffle
     case bip69
 
     var title: String {
         switch self {
-        case .none: return "default"
         case .shuffle: return "shuffle"
         case .bip69: return "deterministic"
         }
