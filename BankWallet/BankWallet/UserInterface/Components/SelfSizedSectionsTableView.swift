16f760c32a2a29cd67a93d657d5b1f606fcb6cb3
Ermat <ealymbaev@gmail.com>
2020-04-27 16:59:59 +0600

    Refactor Unlink module
    - apply dynamic height for checkbox rows
    - make title and delete button simple views, instead of table view rows
    - apply new button styles

--- BankWallet/BankWallet/UserInterface/Components/SelfSizedSectionsTableView.swift
+++ BankWallet/BankWallet/UserInterface/Components/SelfSizedSectionsTableView.swift
@@ -4,14 +4,30 @@ import SectionsTableView
 class SelfSizedSectionsTableView: SectionsTableView {
     var maxHeight: CGFloat = UIScreen.main.bounds.size.height
 
+    override init(style: Style) {
+        super.init(style: style)
+
+        backgroundColor = .clear
+        separatorStyle = .none
+        alwaysBounceVertical = false
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func reloadData() {
         super.reloadData()
-        self.invalidateIntrinsicContentSize()
-        self.layoutIfNeeded()
+
+        invalidateIntrinsicContentSize()
+        layoutIfNeeded()
     }
 
     override var intrinsicContentSize: CGSize {
-        let height = min(contentSize.height, maxHeight)
+        setNeedsLayout()
+        layoutIfNeeded()
+
+        let height = min(ceil(contentSize.height), maxHeight)
         return CGSize(width: contentSize.width, height: height)
     }
 
