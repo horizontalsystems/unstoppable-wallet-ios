c11b09ebd6c18bf328f9e33c7da823f251ded6db
Ermat <ealymbaev@gmail.com>
2020-04-29 17:10:30 +0600

    Refactor TransactionInfo module to use sections table view and new action sheet

---
+++ BankWallet/BankWallet/Modules/TransactionInfo/Cells/TransactionInfoTransactionIdCell.swift
@@ -0,0 +1,64 @@
+import UIKit
+import ThemeKit
+
+class TransactionInfoTransactionIdCell: ThemeCell {
+    private let titleView = TransactionInfoTitleView()
+    private let idButton = ThemeButton()
+    private let shareButton = ThemeButton()
+
+    private var onTapId: (() -> ())?
+    private var onTapShare: (() -> ())?
+
+    override init(style: CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        contentView.addSubview(titleView)
+        titleView.snp.makeConstraints { maker in
+            maker.leading.top.bottom.equalToSuperview()
+        }
+
+        titleView.bind(text: "tx_info.transaction_id".localized)
+
+        contentView.addSubview(idButton)
+        idButton.snp.makeConstraints { maker in
+            maker.leading.equalTo(titleView.snp.trailing)
+            maker.centerY.equalToSuperview()
+        }
+
+        idButton.apply(style: .secondaryDefault)
+        idButton.addTarget(self, action: #selector(_onTapId), for: .touchUpInside)
+
+        contentView.addSubview(shareButton)
+        shareButton.snp.makeConstraints { maker in
+            maker.leading.equalTo(idButton.snp.trailing).offset(CGFloat.margin1x)
+            maker.trailing.equalToSuperview().inset(CGFloat.margin4x)
+            maker.centerY.equalToSuperview()
+        }
+
+        shareButton.apply(style: .secondaryIcon)
+        shareButton.apply(secondaryIconImage: UIImage(named: "Share Transaction Icon"))
+        shareButton.addTarget(self, action: #selector(_onTapShare), for: .touchUpInside)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @objc private func _onTapId() {
+        onTapId?()
+    }
+
+    @objc private func _onTapShare() {
+        onTapShare?()
+    }
+
+    func bind(value: String, onTapId: @escaping () -> (), onTapShare: @escaping () -> ()) {
+        super.bind(bottomSeparatorVisible: true)
+
+        idButton.setTitle(value, for: .normal)
+
+        self.onTapId = onTapId
+        self.onTapShare = onTapShare
+    }
+
+}
--- BankWallet/BankWallet/Modules/TransactionInfo/TransactionInfoRouter.swift
+++ BankWallet/BankWallet/Modules/TransactionInfo/TransactionInfoRouter.swift
@@ -3,24 +3,33 @@ import ActionSheet
 
 class TransactionInfoRouter {
     weak var viewController: UIViewController?
+    private weak var sourceViewController: UIViewController?
+
+    init(sourceViewController: UIViewController?) {
+        self.sourceViewController = sourceViewController
+    }
+
 }
 
 extension TransactionInfoRouter: ITransactionInfoRouter {
 
-    func openFullInfo(transactionHash: String, wallet: Wallet) {
-        viewController?.present(FullTransactionInfoRouter.module(transactionHash: transactionHash, wallet: wallet), animated: true)
+    func showFullInfo(transactionHash: String, wallet: Wallet) {
+        let module = FullTransactionInfoRouter.module(transactionHash: transactionHash, wallet: wallet)
+        viewController?.dismiss(animated: true) { [weak self] in
+            self?.sourceViewController?.present(module, animated: true)
+        }
     }
 
-    func openLockInfo() {
+    func showLockInfo() {
         viewController?.present(InfoRouter.module(title: "lock_info.title".localized, text: "lock_info.text".localized), animated: true)
     }
 
-    func share(value: String) {
+    func showShare(value: String) {
         let activityViewController = UIActivityViewController(activityItems: [value], applicationActivities: [])
-        viewController?.present(activityViewController, animated: true, completion: nil)
+        viewController?.present(activityViewController, animated: true)
     }
 
-    func openDoubleSpendInfo(txHash: String, conflictingTxHash: String?) {
+    func showDoubleSpendInfo(txHash: String, conflictingTxHash: String?) {
         viewController?.present(DoubleSpendInfoRouter.module(txHash: txHash, conflictingTxHash: conflictingTxHash), animated: true)
     }
 
@@ -28,18 +37,16 @@ extension TransactionInfoRouter: ITransactionInfoRouter {
 
 extension TransactionInfoRouter {
 
-    static func module(viewItem: TransactionViewItem) -> ActionSheetController {
-        let router = TransactionInfoRouter()
-
+    static func module(viewItem: TransactionViewItem, sourceViewController: UIViewController?) -> UIViewController {
+        let router = TransactionInfoRouter(sourceViewController: sourceViewController)
         let interactor = TransactionInfoInteractor(pasteboardManager: App.shared.pasteboardManager)
-        let presenter = TransactionInfoPresenter(interactor: interactor, router: router, viewItem: viewItem)
+        let presenter = TransactionInfoPresenter(viewItem: viewItem, interactor: interactor, router: router)
         let viewController = TransactionInfoViewController(delegate: presenter)
 
-        interactor.delegate = presenter
         presenter.view = viewController
         router.viewController = viewController
 
-        return viewController
+        return viewController.toBottomSheet
     }
 
 }
