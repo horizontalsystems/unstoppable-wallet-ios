2c519804a69632f60941fd583002bce2f94429d8
max <trickster77777@gmail.com>
2020-05-18 18:00:14 +0600

    New buttons on transactions filter bar

--- BankWallet/BankWallet/Modules/Transactions/Views/TransactionsCurrencyCell.swift
+++ BankWallet/BankWallet/Modules/Transactions/Views/TransactionsCurrencyCell.swift
@@ -1,32 +1,22 @@
 import UIKit
+import ThemeKit
 import SnapKit
 
 class TransactionsCurrencyCell: UICollectionViewCell {
-    private static let nameLabelFont = UIFont.subhead2
-    private let roundedView = UIView()
-    private let nameLabel = UILabel()
-    private let verticalMargin: CGFloat = 6
+    private static let nameLabelFont = UIFont.subhead1
+
+    private let button = ThemeButton()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        contentView.addSubview(roundedView)
-        roundedView.addSubview(nameLabel)
-
-        roundedView.snp.makeConstraints { maker in
-            maker.edges.equalToSuperview().inset(UIEdgeInsets(top: verticalMargin, left: 0, bottom: verticalMargin, right: 0))
+        contentView.addSubview(button)
+        button.snp.makeConstraints { maker in
+            maker.edges.equalToSuperview()
         }
 
-        roundedView.layer.cornerRadius = 14             // (TransactionCurrenciesHeaderView.headerHeight - .margin2x * 2) / 2
-        roundedView.layer.borderColor = UIColor.themeSteel20.cgColor
-        roundedView.layer.borderWidth = .heightOneDp
-        roundedView.clipsToBounds = true
-
-        nameLabel.snp.makeConstraints { maker in
-            maker.center.equalToSuperview()
-        }
-
-        nameLabel.font = TransactionsCurrencyCell.nameLabelFont
+        button.apply(style: .secondaryTransparent)
+        button.isUserInteractionEnabled = false
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -34,30 +24,39 @@ class TransactionsCurrencyCell: UICollectionViewCell {
     }
 
     override var isSelected: Bool {
-        get {
-            super.isSelected
+        didSet {
+            bind(selected: isSelected)
         }
-        set {
-            super.isSelected = newValue
-            bind(selected: newValue)
+    }
+
+
+    override var isHighlighted: Bool {
+        didSet {
+            bind(highlighted: isHighlighted)
         }
     }
 
     func bind(title: String, selected: Bool) {
-        nameLabel.text = title
+        button.setTitle(title, for: .normal)
 
         bind(selected: selected)
     }
 
-    func bind(selected: Bool) {
-        nameLabel.textColor = selected ? .themeDark : .themeOz
-        roundedView.backgroundColor = selected ? .themeYellowD : .themeJeremy
+    private func bind(selected: Bool) {
+        button.isSelected = selected
     }
 
+    private func bind(highlighted: Bool) {
+        button.isHighlighted = highlighted
+    }
+
+}
+
+extension TransactionsCurrencyCell {
     static func size(for title: String) -> CGSize {
         let size = title.size(containerWidth: .greatestFiniteMagnitude, font: TransactionsCurrencyCell.nameLabelFont)
-        let calculatedWidth = size.width + 2 * .margin8x
-        return CGSize(width: max(calculatedWidth, 80), height: TransactionCurrenciesHeaderView.headerHeight)
+        let calculatedWidth = size.width + 2 * .margin3x
+        return CGSize(width: calculatedWidth, height: 28)
     }
 
 }
