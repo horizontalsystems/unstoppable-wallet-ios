a76dfed290769d81630be7c42e69cbd29874150d
Ermat <ealymbaev@gmail.com>
2020-06-02 12:39:05 +0600

    Extract PrivacyHeaderCell into HighlightedDescriptionCell
    - integrate HighlightedDescriptionCell into AddErc20Token module

---
+++ UnstoppableWallet/UnstoppableWallet/UserInterface/Cells/HighlightedDescriptionCell.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+class HighlightedDescriptionCell: UITableViewCell {
+    private static let horizontalMargin: CGFloat = .margin4x
+    private static let verticalMargin: CGFloat = .margin3x
+
+    private let descriptionView = HighlightedDescriptionView()
+
+    override public init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        backgroundColor = .clear
+        selectionStyle = .none
+
+        contentView.addSubview(descriptionView)
+        descriptionView.snp.makeConstraints { maker in
+            maker.leading.trailing.equalToSuperview().inset(HighlightedDescriptionCell.horizontalMargin)
+            maker.top.equalToSuperview().offset(HighlightedDescriptionCell.verticalMargin)
+        }
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func bind(text: String?) {
+        descriptionView.bind(text: text)
+    }
+
+}
+
+extension HighlightedDescriptionCell {
+
+    static func height(containerWidth: CGFloat, text: String) -> CGFloat {
+        let descriptionViewWidth = containerWidth - 2 * horizontalMargin
+        let descriptionViewHeight = HighlightedDescriptionView.height(containerWidth: descriptionViewWidth, text: text)
+        return descriptionViewHeight + 2 * verticalMargin
+    }
+
+}
