865e06dc6ba11fb04bfffd92ea0f162fec218b0c
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2020-08-25 17:52:05 +0600

    Refactor SwapApproveModule
    - Use synchronous outputs in service
    - Move all states to service

--- UnstoppableWallet/UnstoppableWallet/Modules/Swap/SwapModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Swap/SwapModule.swift
@@ -11,6 +11,10 @@ func subscribe<T>(_ disposeBag: DisposeBag, _ driver: Driver<T>, _ onNext: ((T) 
     driver.drive(onNext: onNext).disposed(by: disposeBag)
 }
 
+func subscribe<T>(_ disposeBag: DisposeBag, _ signal: Signal<T>, _ onNext: ((T) -> Void)? = nil) {
+    signal.emit(onNext: onNext).disposed(by: disposeBag)
+}
+
 func subscribe<T>(_ disposeBag: DisposeBag, _ observable: Observable<T>, _ onNext: ((T) -> Void)? = nil) {
     observable.subscribe(onNext: onNext).disposed(by: disposeBag)
 }
