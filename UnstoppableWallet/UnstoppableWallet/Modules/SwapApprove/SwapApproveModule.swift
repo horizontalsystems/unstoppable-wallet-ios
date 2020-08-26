f14af3d7d5ee07400957ebbf082ec477ad2116f7
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2020-08-26 18:48:40 +0600

    SwapApproveModule: Check balance sufficiency in FeeService

--- UnstoppableWallet/UnstoppableWallet/Modules/SwapApprove/SwapApproveModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/SwapApprove/SwapApproveModule.swift
@@ -6,7 +6,8 @@ struct SwapApproveModule {
     static func instance(coin: Coin, spenderAddress: Address, amount: Decimal, delegate: ISwapApproveDelegate) -> UIViewController? {
         guard let wallet = App.shared.walletManager.wallets.first(where: { $0.coin == coin }),
               let erc20Adapter = App.shared.adapterManager.adapter(for: wallet) as? IErc20Adapter,
-              let feePresenter = FeeModule.instance(erc20Adapter: erc20Adapter, coin: coin, amount: amount, spenderAddress: spenderAddress) else {
+              let balanceAdapter = App.shared.adapterManager.balanceAdapter(for: wallet),
+              let feePresenter = FeeModule.instance(erc20Adapter: erc20Adapter, balanceAdapter: balanceAdapter, coin: coin, amount: amount, spenderAddress: spenderAddress) else {
 
             return nil
         }
