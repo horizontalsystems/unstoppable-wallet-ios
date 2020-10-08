cd058fca563f815eff2254f019d1e3ff9aad38a2
max <trickster77777@gmail.com>
2020-10-08 17:54:23 +0600

    Refactor buttons layout update

--- UnstoppableWallet/UnstoppableWallet/Modules/WalletConnect/Main/WalletConnectMainViewController.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/WalletConnect/Main/WalletConnectMainViewController.swift
@@ -1,12 +1,14 @@
 import ThemeKit
 import RxSwift
 import RxCocoa
+import UIExtensions
 import HUD
 import SectionsTableView
+import SnapKit
 
 class WalletConnectMainViewController: ThemeViewController {
-    static let spinnerLineWidth: CGFloat = 2
-    static let spinnerSideSize: CGFloat = 20
+    private static let spinnerLineWidth: CGFloat = 2
+    private static let spinnerSideSize: CGFloat = 20
 
     private let viewModel: WalletConnectViewModel
     private let presenter: WalletConnectMainPresenter
@@ -18,11 +20,23 @@ class WalletConnectMainViewController: ThemeViewController {
 
     private let peerMetaLabel = UILabel()
 
-    private let buttonsHolder = UIView()
-    private let cancelButton = ThemeButton()
-    private let approveButton = ThemeButton()
-    private let rejectButton = ThemeButton()
+    private let buttonsHolder = GradientView(gradientHeight: .margin4x, fromColor: UIColor.themeTyler.withAlphaComponent(0), toColor: .themeTyler)
+
     private let disconnectButton = ThemeButton()
+    private var disconnectButtonBottomConstraint: Constraint?
+    private var disconnectButtonHeightConstraint: Constraint?
+
+    private let rejectButton = ThemeButton()
+    private var rejectButtonBottomConstraint: Constraint?
+    private var rejectButtonHeightConstraint: Constraint?
+
+    private let approveButton = ThemeButton()
+    private var approveButtonBottomConstraint: Constraint?
+    private var approveButtonHeightConstraint: Constraint?
+
+    private let cancelButton = ThemeButton()
+    private var cancelButtonBottomConstraint: Constraint?
+    private var cancelButtonHeightConstraint: Constraint?
 
     private let tableView = SectionsTableView(style: .grouped)
 
@@ -66,56 +80,63 @@ class WalletConnectMainViewController: ThemeViewController {
 
         view.addSubview(buttonsHolder)
         buttonsHolder.snp.makeConstraints { maker in
-            maker.leading.trailing.equalToSuperview().inset(CGFloat.margin6x)
-            maker.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(CGFloat.margin6x)
-            maker.height.equalTo(CGFloat.heightButton)
-        }
-
-        buttonsHolder.addSubview(cancelButton)
-        cancelButton.snp.makeConstraints { maker in
-            maker.top.leading.trailing.equalToSuperview()
-            maker.height.equalTo(CGFloat.heightButton)
-        }
-
-        cancelButton.apply(style: .primaryGray)
-        cancelButton.setTitle("Cancel", for: .normal)
-        cancelButton.addTarget(self, action: #selector(onTapCancel), for: .touchUpInside)
-
-        buttonsHolder.addSubview(approveButton)
-        approveButton.snp.makeConstraints { maker in
-            maker.top.leading.trailing.equalToSuperview()
-            maker.height.equalTo(CGFloat.heightButton)
-        }
-
-        approveButton.apply(style: .primaryYellow)
-        approveButton.setTitle("Approve", for: .normal)
-        approveButton.addTarget(self, action: #selector(onTapApprove), for: .touchUpInside)
-
-        buttonsHolder.addSubview(rejectButton)
-        rejectButton.snp.makeConstraints { maker in
-            maker.top.equalTo(approveButton.snp.bottom).offset(CGFloat.margin4x)
             maker.leading.trailing.equalToSuperview()
-            maker.height.equalTo(CGFloat.heightButton)
+            maker.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
         }
-
-        rejectButton.apply(style: .primaryGray)
-        rejectButton.setTitle("Reject", for: .normal)
-        rejectButton.addTarget(self, action: #selector(onTapReject), for: .touchUpInside)
 
         buttonsHolder.addSubview(disconnectButton)
         disconnectButton.snp.makeConstraints { maker in
-            maker.top.leading.trailing.equalToSuperview()
-            maker.height.equalTo(CGFloat.heightButton)
+            maker.leading.trailing.equalToSuperview().inset(CGFloat.margin6x)
+
+            disconnectButtonBottomConstraint = maker.bottom.equalToSuperview().offset(-CGFloat.margin4x).constraint
+            disconnectButtonHeightConstraint = maker.height.equalTo(CGFloat.heightButton).constraint
         }
 
         disconnectButton.apply(style: .primaryRed)
         disconnectButton.setTitle("Disconnect", for: .normal)
         disconnectButton.addTarget(self, action: #selector(onTapDisconnect), for: .touchUpInside)
 
+        buttonsHolder.addSubview(rejectButton)
+        rejectButton.snp.makeConstraints { maker in
+            maker.leading.trailing.equalToSuperview().inset(CGFloat.margin6x)
+
+            rejectButtonBottomConstraint = maker.bottom.equalTo(disconnectButton.snp.top).offset(-CGFloat.margin4x).constraint
+            rejectButtonHeightConstraint = maker.height.equalTo(CGFloat.heightButton).constraint
+        }
+
+        rejectButton.apply(style: .primaryGray)
+        rejectButton.setTitle("Reject", for: .normal)
+        rejectButton.addTarget(self, action: #selector(onTapReject), for: .touchUpInside)
+
+        buttonsHolder.addSubview(approveButton)
+        approveButton.snp.makeConstraints { maker in
+            maker.leading.trailing.equalToSuperview().inset(CGFloat.margin6x)
+
+            approveButtonBottomConstraint = maker.bottom.equalTo(rejectButton.snp.top).offset(-CGFloat.margin4x).constraint
+            approveButtonHeightConstraint = maker.height.equalTo(CGFloat.heightButton).constraint
+        }
+
+        approveButton.apply(style: .primaryYellow)
+        approveButton.setTitle("Approve", for: .normal)
+        approveButton.addTarget(self, action: #selector(onTapApprove), for: .touchUpInside)
+
+        buttonsHolder.addSubview(cancelButton)
+        cancelButton.snp.makeConstraints { maker in
+            maker.top.equalToSuperview().inset(CGFloat.margin8x)
+            maker.leading.trailing.equalToSuperview().inset(CGFloat.margin6x)
+
+            cancelButtonBottomConstraint = maker.bottom.equalTo(approveButton.snp.top).offset(-CGFloat.margin4x).constraint
+            cancelButtonHeightConstraint = maker.height.equalTo(CGFloat.heightButton).constraint
+        }
+
+        cancelButton.apply(style: .primaryGray)
+        cancelButton.setTitle("Cancel", for: .normal)
+        cancelButton.addTarget(self, action: #selector(onTapCancel), for: .touchUpInside)
+
         view.addSubview(tableView)
         tableView.snp.makeConstraints { maker in
             maker.leading.top.trailing.equalToSuperview()
-            maker.bottom.equalTo(buttonsHolder.snp.top)
+            maker.bottom.equalTo(buttonsHolder.snp.top).offset(CGFloat.margin4x)
         }
 
         tableView.sectionDataSource = self
@@ -131,19 +152,20 @@ class WalletConnectMainViewController: ThemeViewController {
 
         presenter.cancelVisibleDriver
                 .drive(onNext: { [weak self] visible in
-                    self?.syncButtons(state: .cancel)
+                    self?.syncButtonConstraints(bottom: self?.cancelButtonBottomConstraint, height: self?.cancelButtonHeightConstraint, visible: visible)
                 })
                 .disposed(by: disposeBag)
 
         presenter.approveAndRejectVisibleDriver
                 .drive(onNext: { [weak self] visible in
-                    self?.syncButtons(state: .approveReject)
+                    self?.syncButtonConstraints(bottom: self?.approveButtonBottomConstraint, height: self?.approveButtonHeightConstraint, visible: visible)
+                    self?.syncButtonConstraints(bottom: self?.rejectButtonBottomConstraint, height: self?.rejectButtonHeightConstraint, visible: visible)
                 })
                 .disposed(by: disposeBag)
 
         presenter.disconnectVisibleDriver
                 .drive(onNext: { [weak self] visible in
-                    self?.syncButtons(state: .disconnect)
+                    self?.syncButtonConstraints(bottom: self?.disconnectButtonBottomConstraint, height: self?.disconnectButtonHeightConstraint, visible: visible)
                 })
                 .disposed(by: disposeBag)
 
@@ -199,30 +221,9 @@ class WalletConnectMainViewController: ThemeViewController {
         }
     }
 
-    private func syncButtons(state: ButtonsState) {
-        cancelButton.isHidden = true
-        approveButton.isHidden = true
-        rejectButton.isHidden = true
-        disconnectButton.isHidden = true
-
-        switch state {
-        case .cancel:
-            buttonsHolder.snp.updateConstraints { maker in
-                maker.height.equalTo(CGFloat.heightButton)
-            }
-            cancelButton.isHidden = false
-        case .approveReject:
-            buttonsHolder.snp.updateConstraints { maker in
-                maker.height.equalTo(CGFloat.heightButton * 2 + CGFloat.margin4x)
-            }
-            approveButton.isHidden = false
-            rejectButton.isHidden = false
-        case .disconnect:
-            buttonsHolder.snp.updateConstraints { maker in
-                maker.height.equalTo(CGFloat.heightButton)
-            }
-            disconnectButton.isHidden = false
-        }
+    private func syncButtonConstraints(bottom: Constraint?, height: Constraint?, visible: Bool) {
+        bottom?.update(offset: visible ? -CGFloat.margin4x : 0)
+        height?.update(offset: visible ? CGFloat.heightButton : 0)
     }
 
     @objc private func onTapCancel() {
@@ -283,12 +284,4 @@ extension WalletConnectMainViewController: SectionsDataSource {
         []
     }
 
-}
-
-extension WalletConnectMainViewController {
-    enum ButtonsState {
-        case cancel
-        case approveReject
-        case disconnect
-    }
 }
