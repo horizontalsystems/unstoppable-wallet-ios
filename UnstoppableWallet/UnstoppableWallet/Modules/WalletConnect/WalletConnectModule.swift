332c39c38974dcdd5355fb384ff0f5a230b92d13
Ermat <ealymbaev@gmail.com>
2020-10-26 14:36:48 +0600

    Wallet Connect: integrate reachability manager to service

--- UnstoppableWallet/UnstoppableWallet/Modules/WalletConnect/WalletConnectModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/WalletConnect/WalletConnectModule.swift
@@ -5,7 +5,8 @@ struct WalletConnectModule {
     static func start(sourceViewController: UIViewController?) {
         let service = WalletConnectService(
                 ethereumKitManager: App.shared.ethereumKitManager,
-                sessionStore: App.shared.walletConnectSessionStore
+                sessionStore: App.shared.walletConnectSessionStore,
+                reachabilityManager: App.shared.reachabilityManager
         )
         let viewModel = WalletConnectViewModel(service: service)
         let view = WalletConnectView(viewModel: viewModel, sourceViewController: sourceViewController)
