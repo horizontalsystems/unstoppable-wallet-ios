ec627db439cbd072f3c92f5462b22c6831ad1cb9
Ermat <ealymbaev@gmail.com>
2020-11-04 12:34:28 +0600

    Wallet Connect: handle deleting stored session when account is deleted from app

--- UnstoppableWallet/UnstoppableWallet/Modules/WalletConnect/WalletConnectSessionStore.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/WalletConnect/WalletConnectSessionStore.swift
@@ -3,7 +3,41 @@ import RxSwift
 import RxRelay
 
 class WalletConnectSessionStore {
+    private let accountManager: IAccountManager
+    private let predefinedAccountTypeManager: IPredefinedAccountTypeManager
+
     private let storedPeerMetaRelay = PublishRelay<WCPeerMeta?>()
+    private let disposeBag = DisposeBag()
+
+    init(accountManager: IAccountManager, predefinedAccountTypeManager: IPredefinedAccountTypeManager) {
+        self.accountManager = accountManager
+        self.predefinedAccountTypeManager = predefinedAccountTypeManager
+
+        accountManager.deleteAccountObservable
+                .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .background))
+                .subscribe(onNext: { [weak self] account in
+                    self?.handleDeleted(account: account)
+                })
+                .disposed(by: disposeBag)
+    }
+
+    private func handleDeleted(account: Account) {
+        // since there is no multi-account support yet, we delete WC stored session for account that satisfies Standard predefined account type
+
+        guard let predefinedAccountType = predefinedAccountTypeManager.predefinedAccountType(accountType: account.type) else {
+            return
+        }
+
+        guard predefinedAccountType == .standard else {
+            return
+        }
+
+        clear()
+    }
+
+}
+
+extension WalletConnectSessionStore {
 
     var storedItem: WCSessionStoreItem? {
         WCSessionStore.allSessions.first?.value
