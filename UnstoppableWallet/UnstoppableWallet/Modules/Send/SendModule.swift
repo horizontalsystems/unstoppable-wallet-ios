34cc04b098927e847d6f6e7ff5dd6bcf14fcb97b
Ermat <ealymbaev@gmail.com>
2021-01-05 17:40:44 +0600

    Add domain to Send Confirmation module

--- UnstoppableWallet/UnstoppableWallet/Modules/Send/SendModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Send/SendModule.swift
@@ -202,8 +202,17 @@ protocol ISendConfirmationViewItemNew {
 
 struct SendConfirmationAmountViewItem: ISendConfirmationViewItemNew {
     let primaryInfo: AmountInfo
-    var secondaryInfo: AmountInfo?
+    let secondaryInfo: AmountInfo?
     let receiver: Address
+    let isAccount: Bool
+
+    init(primaryInfo: AmountInfo, secondaryInfo: AmountInfo?, receiver: Address, isAccount: Bool = false) {
+        self.primaryInfo = primaryInfo
+        self.secondaryInfo = secondaryInfo
+        self.receiver = receiver
+        self.isAccount = isAccount
+    }
+
 }
 
 struct SendConfirmationFeeViewItem: ISendConfirmationViewItemNew {
