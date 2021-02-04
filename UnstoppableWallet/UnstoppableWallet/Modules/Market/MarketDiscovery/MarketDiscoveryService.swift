4ce019fb6931a3867a76e4da24b686256bb28e03
Ermat <ealymbaev@gmail.com>
2021-02-04 17:24:46 +0600

    Refactor market list modules: extract common logic to MarketList module and use it in MarketDiscovery and MarketWatchlist modules

--- UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketDiscovery/MarketDiscoveryService.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketDiscovery/MarketDiscoveryService.swift
@@ -1,118 +1,71 @@
-import CurrencyKit
 import XRatesKit
 import RxSwift
 import RxRelay
 
 class MarketDiscoveryService {
-    private let currencyKit: ICurrencyKit
     private let rateManager: IRateManager
     private let categoriesProvider: MarketCategoriesProvider
-    private var disposeBag = DisposeBag()
 
-    private let stateRelay = PublishRelay<State>()
-    private(set) var state: State = .loading {
-        didSet {
-            stateRelay.accept(state)
-        }
-    }
-
-    private(set) var items = [MarketModule.Item]()
+    private let refetchRelay = PublishRelay<()>()
 
     private let currentCategoryRelay = PublishRelay<MarketDiscoveryFilter?>()
     var currentCategory: MarketDiscoveryFilter? {
         didSet {
             currentCategoryRelay.accept(currentCategory)
-            items = []
-            fetch()
+            refetchRelay.accept(())
         }
     }
 
-    init(currencyKit: ICurrencyKit, rateManager: IRateManager, categoriesProvider: MarketCategoriesProvider) {
-        self.currencyKit = currencyKit
+    init(rateManager: IRateManager, categoriesProvider: MarketCategoriesProvider) {
         self.rateManager = rateManager
         self.categoriesProvider = categoriesProvider
-
-        fetch()
     }
 
-    private func fetch() {
-        disposeBag = DisposeBag()
+}
 
-        state = .loading
+extension MarketDiscoveryService: IMarketListFetcher {
 
-        let single: Single<[CoinMarket]>
-
+    func fetchSingle(currencyCode: String) -> Single<[MarketModule.Item]> {
         if let category = currentCategory {
 //            let coinCodes = categoriesProvider.coinCodes(for: category == .rated ? nil : category.rawValue)
             let coinCodes = categoriesProvider.coinCodes(for: category.rawValue)
-            single = rateManager.coinsMarketSingle(currencyCode: currencyKit.baseCurrency.code, coinCodes: coinCodes)
-        } else {
-            single = rateManager.topMarketsSingle(currencyCode: currencyKit.baseCurrency.code, itemCount: 250)
-        }
-
-        single
-                .subscribe(onSuccess: { [weak self] coinMarkets in
-                    self?.onFetchSuccess(coinMarkets: coinMarkets)
-                }, onError: { [weak self] error in
-                    self?.onFetchFailed(error: error)
-                })
-                .disposed(by: disposeBag)
-    }
-
-    private func onFetchSuccess(coinMarkets: [CoinMarket]) {
-        items = coinMarkets.enumerated().compactMap { index, coinMarket in
-            let score: MarketModule.Score?
-            switch currentCategory {
-//            case .rated:
-//                guard let rate = categoriesProvider.rate(for: coinMarket.coin.code), !rate.isEmpty else {
-//                    return nil
-//                }
+            return rateManager.coinsMarketSingle(currencyCode: currencyCode, coinCodes: coinCodes)
+                    .map { coinMarkets in
+                        coinMarkets.compactMap { coinMarket in
+                            let score: MarketModule.Score?
+                            switch category {
+//                            case .rated:
+//                                guard let rate = self?.categoriesProvider.rate(for: coinMarket.coin.code), !rate.isEmpty else {
+//                                    return nil
+//                                }
 //
-//                score = .rating(rate)
-            case .none:
-                score = .rank(index + 1)
-            default:
-                score = nil
-            }
-            return MarketModule.Item(coinMarket: coinMarket, score: score)
+//                                score = .rating(rate)
+                            default:
+                                score = nil
+                            }
+                            return MarketModule.Item(coinMarket: coinMarket, score: score)
+                        }
+                    }
+        } else {
+            return rateManager.topMarketsSingle(currencyCode: currencyCode, itemCount: 250)
+                    .map { coinMarkets in
+                        coinMarkets.enumerated().compactMap { index, coinMarket in
+                            MarketModule.Item(coinMarket: coinMarket, score: .rank(index + 1))
+                        }
+                    }
         }
-
-        state = .loaded
     }
 
-    private func onFetchFailed(error: Error) {
-        state = .failed(error: error)
+    var refetchObservable: Observable<()> {
+        refetchRelay.asObservable()
     }
 
 }
 
 extension MarketDiscoveryService {
-
-    var currency: Currency {
-        //todo: refactor to use current currency and handle changing
-        currencyKit.currencies.first { $0.code == "USD" } ?? currencyKit.currencies[0]
-    }
-
-    var stateObservable: Observable<State> {
-        stateRelay.asObservable()
-    }
 
     var currentCategoryObservable: Observable<MarketDiscoveryFilter?> {
         currentCategoryRelay.asObservable()
-    }
-
-    func refresh() {
-        fetch()
-    }
-
-}
-
-extension MarketDiscoveryService {
-
-    enum State {
-        case loaded
-        case loading
-        case failed(error: Error)
     }
 
 }
--- UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketWatchlist/MarketWatchlistViewController.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketWatchlist/MarketWatchlistViewController.swift
@@ -1,26 +1,11 @@
 import UIKit
-import RxSwift
 import ThemeKit
-import SectionsTableView
 
-class MarketWatchlistViewController: ThemeViewController {
-    private let viewModel: MarketWatchlistViewModel
-    private let disposeBag = DisposeBag()
-
-    private let tableView = SectionsTableView(style: .plain)
-    private let headerView = MarketListHeaderView()
-    private let refreshControl = UIRefreshControl()
-
+class MarketWatchlistViewController: MarketListViewController {
     private let cautionCell = CautionCell()
 
-    weak var parentNavigationController: UINavigationController?
-
-    private var state: MarketWatchlistViewModel.State = .loading
-
-    init(viewModel: MarketWatchlistViewModel) {
-        self.viewModel = viewModel
-
-        super.init()
+    init(listViewModel: MarketListViewModel) {
+        super.init(listViewModel: listViewModel)
     }
 
     required init?(coder: NSCoder) {
@@ -30,146 +15,12 @@ class MarketWatchlistViewController: ThemeViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        refreshControl.tintColor = .themeLeah
-        refreshControl.alpha = 0.6
-        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
-
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { maker in
-            maker.edges.equalToSuperview()
-        }
-
-        tableView.separatorStyle = .none
-        tableView.backgroundColor = .clear
-
-        tableView.registerHeaderFooter(forClass: MarketListHeaderView.self)
-        tableView.registerCell(forClass: G14Cell.self)
-
-        tableView.sectionDataSource = self
-
         cautionCell.cautionImage = UIImage(named: "rate_48")
         cautionCell.cautionText = "market_watchlist.empty.caption".localized
-
-        headerView.onTapSortField = { [weak self] in
-            self?.onTapSortingField()
-        }
-        headerView.onSelect = { [weak self] field in
-            self?.viewModel.set(marketField: field)
-        }
-
-        subscribe(disposeBag, viewModel.stateDriver) { [weak self] state in
-            self?.state = state
-            self?.tableView.reload()
-        }
-        subscribe(disposeBag, viewModel.sortingFieldTitleDriver) { [weak self] title in
-            self?.headerView.setSortingField(title: title)
-        }
-        subscribe(disposeBag, viewModel.marketFieldDriver) { [weak self] marketField in
-            self?.headerView.setMarketField(field: marketField)
-        }
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-        tableView.refreshControl = refreshControl
-    }
-
-    @objc func onRefresh() {
-        viewModel.refresh()
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
-            self?.refreshControl.endRefreshing()
-        }
-    }
-
-    private func onTapSortingField() {
-        let alertController = AlertRouter.module(
-                title: "market.sort_by".localized,
-                viewItems: viewModel.sortingFieldViewItems.map { viewItem in
-                    AlertViewItem(
-                            text: viewItem.title,
-                            selected: viewItem.selected
-                    )
-                }
-        ) { [weak self] index in
-            self?.viewModel.setSortingField(at: index)
-        }
-
-        present(alertController, animated: true)
-    }
-
-    private func row(viewItem: MarketModule.ViewItem, isLast: Bool) -> RowProtocol {
-        Row<G14Cell>(
-            id: viewItem.coinCode,
-            height: .heightDoubleLineCell,
-            autoDeselect: true,
-            bind: { cell, _ in
-                cell.set(backgroundStyle: .transparent, isLast: isLast)
-                MarketModule.bind(cell: cell, viewItem: viewItem)
-            },
-            action: { [weak self] _ in
-                self?.onSelect(viewItem: viewItem)
-            })
-    }
-
-    private func onSelect(viewItem: MarketModule.ViewItem) {
-        let viewController = ChartRouter.module(launchMode: .partial(coinCode: viewItem.coinCode, coinTitle: viewItem.coinName, coinType: nil))
-        parentNavigationController?.pushViewController(viewController, animated: true)
-    }
-
-}
-
-extension MarketWatchlistViewController: SectionsDataSource {
-
-    func buildSections() -> [SectionProtocol] {
-        let rows: [RowProtocol]
-        var headerState: ViewState<MarketListHeaderView> = .margin(height: 0)
-
-        switch state {
-        case .loading:
-            rows = [
-                Row<SpinnerCell>(
-                        id: "spinner",
-                        dynamicHeight: { [weak self] _ in
-                            max(0, (self?.tableView.height ?? 0) - MarketListHeaderView.height)
-                        }
-                )
-            ]
-
-        case .loaded(let viewItems):
-            if viewItems.isEmpty {
-                rows = [
-                    StaticRow(
-                            cell: cautionCell,
-                            id: "caution",
-                            dynamicHeight: { [weak self] _ in
-                                max(0, (self?.tableView.height ?? 0) - MarketListHeaderView.height)
-                            }
-                    )
-                ]
-            } else {
-                headerState = .static(view: headerView, height: MarketListHeaderView.height)
-                rows = viewItems.enumerated().map { row(viewItem: $1, isLast: $0 == viewItems.count - 1) }
-            }
-
-        case .error(let errorDescription):
-            rows = [
-                Row<ErrorCell>(
-                        id: "error",
-                        dynamicHeight: { [weak self] _ in
-                            max(0, (self?.tableView.height ?? 0) - MarketListHeaderView.height)
-                        },
-                        bind: { cell, _ in
-                            cell.errorText = errorDescription
-                        }
-                )
-            ]
-        }
-
-        return [
-            Section(id: "tokens", headerState: headerState, rows: rows)
-        ]
+    override var emptyCell: UITableViewCell? {
+        cautionCell
     }
 
 }
