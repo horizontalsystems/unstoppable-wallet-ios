5e909b9e7e7ad95cf5b7ff7138cce701f50365c0
Ermat <ealymbaev@gmail.com>
2021-03-18 14:45:21 +0600

    CoinPage module:
    - add links sections and handle opening urls
    - add powered by footer cell
    - add stub cells for Coin Markets and Coin Investors

--- UnstoppableWallet/UnstoppableWallet/Modules/Coin/CoinPageViewModel.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Coin/CoinPageViewModel.swift
@@ -8,6 +8,7 @@ class CoinPageViewModel {
     private let service: CoinPageService
     private let disposeBag = DisposeBag()
 
+    private let viewItemRelay = BehaviorRelay<ViewItem?>(value: nil)
     private let loadingRelay = BehaviorRelay<Bool>(value: false)
 
     init(service: CoinPageService) {
@@ -25,6 +26,26 @@ class CoinPageViewModel {
 
     private func sync(state: DataStatus<CoinMarketInfo>) {
         loadingRelay.accept(state.isLoading)
+
+        let viewItem = state.data.map { info in
+            ViewItem(
+                    links: links(linkMap: info.meta.links)
+            )
+        }
+
+        viewItemRelay.accept(viewItem)
+    }
+
+    private func links(linkMap: [LinkType: String]) -> [Link] {
+        let linkTypes: [LinkType] = [.guide, .website, .whitepaper, .reddit, .twitter, .telegram, .github]
+
+        return linkTypes.compactMap { linkType in
+            guard let url = linkMap[linkType], !url.isEmpty else {
+                return nil
+            }
+
+            return Link(type: linkType, url: url)
+        }
     }
 
 }
@@ -39,8 +60,53 @@ extension CoinPageViewModel {
         service.coinTitle
     }
 
+    var viewItemDriver: Driver<ViewItem?> {
+        viewItemRelay.asDriver()
+    }
+
     var loadingDriver: Driver<Bool> {
         loadingRelay.asDriver()
+    }
+
+    var coinCode: String {
+        service.coinCode
+    }
+
+}
+
+extension CoinPageViewModel {
+
+    struct ViewItem {
+        let links: [Link]
+    }
+
+    struct Link {
+        let type: LinkType
+        let url: String
+
+        var icon: UIImage? {
+            switch type {
+            case .guide: return UIImage(named: "academy_1_20")
+            case .website: return UIImage(named: "globe_20")
+            case .whitepaper: return UIImage(named: "academy_1_20")
+            case .reddit: return UIImage(named: "reddit_20")
+            case .twitter: return UIImage(named: "twitter_20")
+            case .telegram: return UIImage(named: "telegram_20")
+            case .github: return UIImage(named: "github_20")
+            }
+        }
+
+        var title: String {
+            switch type {
+            case .guide: return "coin_page.guide".localized
+            case .website: return "coin_page.website".localized
+            case .whitepaper: return "coin_page.whitepaper".localized
+            case .reddit: return "Reddit"
+            case .twitter: return "Twitter"
+            case .telegram: return "Telegram"
+            case .github: return "Github"
+            }
+        }
     }
 
 }
