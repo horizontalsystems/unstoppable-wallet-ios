dec7c63ab0c3ac4071597ff72dbdcd24620a28db
max <trickster77777@gmail.com>
2021-07-01 13:04:24 +0600

    Fix syntax error

--- UnstoppableWallet/UnstoppableWallet/Modules/SendEvmTransaction/SendEvmTransactionViewController.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/SendEvmTransaction/SendEvmTransactionViewController.swift
@@ -89,7 +89,7 @@ class SendEvmTransactionViewController: ThemeViewController {
     }
 
     func handleSendSuccess(transactionHash: Data) {
-        HudHelper.instance.showSuccess("alert.success_action".localized)
+        HudHelper.instance.showSuccess(title: "alert.success_action".localized)
 
         dismiss(animated: true)
     }
