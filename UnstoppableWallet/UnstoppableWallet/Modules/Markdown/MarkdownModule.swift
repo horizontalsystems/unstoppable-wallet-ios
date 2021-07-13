24de11d4ad0f1b75c0ebeb18590116a63cbcb0c1
max <trickster77777@gmail.com>
2021-07-13 14:32:54 +0600

    Show "follow us" buttons in release notes
    - add local network usage description

--- UnstoppableWallet/UnstoppableWallet/Modules/Markdown/MarkdownModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Markdown/MarkdownModule.swift
@@ -11,13 +11,13 @@ struct MarkdownModule {
         return MarkdownViewController(viewModel: viewModel, handleRelativeUrl: handleRelativeUrl)
     }
 
-    static func gitReleaseNotesMarkdownViewController(url: URL, closeHandler: (() -> ())? = nil) -> UIViewController {
+    static func gitReleaseNotesMarkdownViewController(url: URL) -> UIViewController {
         let provider = MarkdownGitReleaseContentProvider(url: url, networkManager: App.shared.networkManager)
         let service = MarkdownService(provider: provider)
         let parser = MarkdownParser()
         let viewModel = MarkdownViewModel(service: service, parser: parser)
 
-        return MarkdownViewController(viewModel: viewModel, showClose: true, closeHandler: closeHandler, handleRelativeUrl: false)
+        return MarkdownViewController(viewModel: viewModel, handleRelativeUrl: false)
     }
 
 }
