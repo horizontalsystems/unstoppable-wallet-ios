e5aee3037af40060d676980b0f2a70d81bb3ba46
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2021-08-27 11:43:41 +0600

    Add contract address and method labels

--- UnstoppableWallet/UnstoppableWallet/Modules/TransactionInfo/TransactionInfoAddressMapper.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/TransactionInfo/TransactionInfoAddressMapper.swift
@@ -4,6 +4,9 @@ class TransactionInfoAddressMapper {
         "0x7a250d5630b4cf539739df2c5dacb4c659f2488d": "Uniswap v.2",
         "0x11111112542d85b3ef69ae05771c2dccff4faa26": "1Inch V3",
         "0x881d40237659c251811cec9c364ef91dc08d300c": "Metamask: Swap Router",
+        "0xc2EdaD668740f1aA35E4D8f227fB8E17dcA888Cd": "SushiSwap",
+        "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F": "SushiSwap",
+        "0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272": "SushiSwap",
 
         // Binanace Smart Chain
         "0x05ff2b0db69458a0750badebc4f9e13add608c7f": "PancakeSwap",
