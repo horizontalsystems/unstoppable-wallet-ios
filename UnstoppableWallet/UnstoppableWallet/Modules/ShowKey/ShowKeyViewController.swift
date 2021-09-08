dbaa16b8c9f965a7df51680b2db6f6c002aa016f
anton <ant013@mail.ru>
2021-09-08 16:18:02 +0600

    Add warning when copy private key.

--- UnstoppableWallet/UnstoppableWallet/Modules/ShowKey/ShowKeyViewController.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/ShowKey/ShowKeyViewController.swift
@@ -132,6 +132,12 @@ class ShowKeyViewController: ThemeViewController {
         closeButtonHolder.set(hidden: false, animated: true, duration: animationDuration)
     }
 
+    private func handleTap(viewItem: CopyableSecondaryButton.ViewItem) {
+        let viewController = PrivateKeyCopyConfirmationViewController(privateKey: viewItem.value())
+
+        present(viewController.toBottomSheet, animated: true)
+    }
+
     private func marginRow(id: String, height: CGFloat) -> RowProtocol {
         Row<EmptyCell>(id: id, height: height)
     }
@@ -157,9 +163,10 @@ class ShowKeyViewController: ThemeViewController {
                     dynamicHeight: { width in
                         Cell9.height(containerWidth: width, backgroundStyle: .lawrence, viewItem: viewItem)
                     },
-                    bind: { cell, _ in
+                    bind: { [weak self] cell, _ in
                         cell.set(backgroundStyle: .lawrence, isLast: true)
                         cell.viewItem = viewItem
+                        cell.handler = { self?.handleTap(viewItem: $0) }
                     }
             )
         ]
