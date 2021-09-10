01274cc418159692e5e5c6edb98b7845b39ad166
Ermat <ealymbaev@gmail.com>
2021-09-10 18:04:29 +0600

    Switch `MarketSearch` module to new `MarketKit`

--- UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketSearch/MarketSearchModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketSearch/MarketSearchModule.swift
@@ -4,7 +4,7 @@ import UIKit
 class MarketSearchModule {
 
     static func viewController() -> UIViewController {
-        let service = MarketSearchService(rateManager: App.shared.rateManager)
+        let service = MarketSearchService(marketKit: App.shared.marketKit)
         let viewModel = MarketSearchViewModel(service: service)
 
         return ThemeNavigationController(rootViewController: MarketSearchViewController(viewModel: viewModel))
