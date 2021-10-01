f86d1a87839ce2a47e7f714593d7ead5c2abe9c4
Ermat <ealymbaev@gmail.com>
2021-10-01 14:51:05 +0600

    Change opening mode for `MarketDiscovery` module to "push"

--- UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketDiscovery/MarketDiscoveryModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketDiscovery/MarketDiscoveryModule.swift
@@ -1,14 +1,11 @@
 import UIKit
-import ThemeKit
 
 struct MarketDiscoveryModule {
 
     static func viewController() -> UIViewController {
         let service = MarketDiscoveryService(marketKit: App.shared.marketKit)
         let viewModel = MarketDiscoveryViewModel(service: service)
-        let viewController = MarketDiscoveryViewController(viewModel: viewModel)
-
-        return ThemeNavigationController(rootViewController: viewController)
+        return MarketDiscoveryViewController(viewModel: viewModel)
     }
 
 }
