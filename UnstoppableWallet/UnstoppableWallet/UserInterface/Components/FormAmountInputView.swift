ccc8b9bc5d7d173c04bb71c5134c443e73fe2b26
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2021-11-12 18:17:00 +0600

    Show "Price impact" warning on swap screen

--- UnstoppableWallet/UnstoppableWallet/UserInterface/Components/FormAmountInputView.swift
+++ UnstoppableWallet/UnstoppableWallet/UserInterface/Components/FormAmountInputView.swift
@@ -28,6 +28,7 @@ class FormAmountInputView: UIView {
 
         subscribe(disposeBag, viewModel.prefixDriver) { [weak self] in self?.set(prefix: $0) }
         subscribe(disposeBag, viewModel.amountDriver) { [weak self] in self?.set(amount: $0) }
+        subscribe(disposeBag, viewModel.amountWarningDriver) { [weak self] in self?.set(amountWarning: $0) }
         subscribe(disposeBag, viewModel.isMaxEnabledDriver) { [weak self] in self?.amountInputView.maxButtonVisible = $0 }
         subscribe(disposeBag, viewModel.switchEnabledDriver) { [weak self] in self?.amountInputView.secondaryButtonEnabled = $0 }
         subscribe(disposeBag, viewModel.secondaryTextDriver) { [weak self] in self?.set(secondaryText: $0) }
@@ -51,6 +52,10 @@ class FormAmountInputView: UIView {
         }
 
         amountInputView.inputText = amount
+    }
+
+    private func set(amountWarning: String?) {
+        amountInputView.warningText = amountWarning
     }
 
     private func textColor(inputType: AmountInputViewModel.InputType) -> UIColor {
