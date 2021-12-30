a03095a1bb0045ceb56e8e0f8c3128ae12e0b0c2
Ermat <ealymbaev@gmail.com>
2021-12-30 17:11:52 +0600

    Apply new design to onboarding screens

---
+++ UnstoppableWallet/UnstoppableWallet/Modules/Welcome/WelcomeTextView.swift
@@ -0,0 +1,42 @@
+import UIKit
+import SnapKit
+import ThemeKit
+
+class WelcomeTextView: UIView {
+
+    init(title: String, description: String) {
+        super.init(frame: .zero)
+
+        let titleLabel = UILabel()
+
+        addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { maker in
+            maker.leading.trailing.equalToSuperview()
+            maker.top.equalToSuperview()
+        }
+
+        titleLabel.textAlignment = .center
+        titleLabel.font = .title3
+        titleLabel.textColor = .themeSteelLight
+        titleLabel.text = title
+
+        let descriptionLabel = UILabel()
+
+        addSubview(descriptionLabel)
+        descriptionLabel.snp.makeConstraints { maker in
+            maker.leading.trailing.equalToSuperview()
+            maker.top.equalTo(titleLabel.snp.bottom).offset(CGFloat.margin16)
+        }
+
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .center
+        descriptionLabel.font = .body
+        descriptionLabel.textColor = .themeGray
+        descriptionLabel.text = description
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
