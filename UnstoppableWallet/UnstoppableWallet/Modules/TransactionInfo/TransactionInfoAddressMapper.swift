703369d1958168279bdc69aac028cbf9b303a299
_imadia <imanahunovad@gmail.com>
2022-03-02 14:37:12 +0600

    Change DefaultCoinList and FeaturedCoinList

--- UnstoppableWallet/UnstoppableWallet/Modules/TransactionInfo/TransactionInfoAddressMapper.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/TransactionInfo/TransactionInfoAddressMapper.swift
@@ -9,11 +9,14 @@ class TransactionInfoAddressMapper {
         "0x8798249c2e607446efb7ad49ec89dd1865ff4272": "SushiSwap",
         "0x1111111254fb6c44bac0bed2854e76f90643097d": "1Inch V4",
 
-        // Binanace Smart Chain
+        // Binance Smart Chain
         "0x05ff2b0db69458a0750badebc4f9e13add608c7f": "PancakeSwap",
         "0x10ed43c718714eb63d5aa57b78b54704e256024e": "PancakeSwap v.2",
         "0xf84e3809971798bd372aecdc03ae977759a619ab": "Bunny Compensation Pool",
         "0xcadc8cb26c8c7cb46500e61171b5f27e9bd7889d": "Pancake Bunny: Bunny Pool",
+
+        // Polygon
+        "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff": "QuickSwap",
     ]
 
     static func map(_ address: String) -> String {
