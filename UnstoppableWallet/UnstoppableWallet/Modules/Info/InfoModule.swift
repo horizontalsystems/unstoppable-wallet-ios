3773b7687117fae715404ab181b25307b6d5b3b2
Ermat <ealymbaev@gmail.com>
2022-05-11 16:21:39 +0600

    `CoinDetails` module: add info buttons to section headers

--- UnstoppableWallet/UnstoppableWallet/Modules/Info/InfoModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Info/InfoModule.swift
@@ -90,4 +90,68 @@ extension InfoModule {
         )
     }
 
+    static var tokenLiquidityInfo: UIViewController {
+        viewController(
+                viewItems: [
+                    .header2(text: "coin_page.token_liquidity".localized),
+                    .text(text: "coin_page.token_liquidity.description".localized),
+                    .header(text: "coin_page.dex_volume".localized),
+                    .text(text: "coin_page.dex_volume.description".localized),
+                    .header(text: "coin_page.dex_liquidity".localized),
+                    .text(text: "coin_page.dex_liquidity.description".localized)
+                ]
+        )
+    }
+
+    static var tokenDistributionInfo: UIViewController {
+        viewController(
+                viewItems: [
+                    .header2(text: "coin_page.token_distribution".localized),
+                    .text(text: "coin_page.token_distribution.description".localized),
+                    .header(text: "coin_page.tx_count".localized),
+                    .text(text: "coin_page.tx_count.description".localized),
+                    .header(text: "coin_page.tx_volume".localized),
+                    .text(text: "coin_page.tx_volume.description".localized),
+                    .header(text: "coin_page.active_addresses".localized),
+                    .text(text: "coin_page.active_addresses.description".localized),
+                    .header(text: "coin_page.major_holders".localized),
+                    .text(text: "coin_page.major_holders.description".localized)
+                ]
+        )
+    }
+
+    static var tokenTvlInfo: UIViewController {
+        viewController(
+                viewItems: [
+                    .header2(text: "coin_page.token_tvl".localized),
+                    .text(text: "coin_page.token_tvl.description".localized),
+                    .header(text: "coin_page.tvl_rank".localized),
+                    .text(text: "coin_page.tvl_rank.description".localized),
+                    .header(text: "coin_page.market_cap_tvl_ratio".localized),
+                    .text(text: "coin_page.market_cap_tvl_ratio.description".localized)
+                ]
+        )
+    }
+
+    static var securityParametersInfo: UIViewController {
+        viewController(
+                viewItems: [
+                    .header2(text: "coin_page.security_parameters".localized),
+                    .header(text: CoinDetailsViewModel.SecurityType.privacy.title),
+                    .text(text: CoinDetailsViewModel.SecurityLevel.high.title.uppercased() + ":\n" + CoinDetailsViewModel.SecurityLevel.high.description),
+                    .text(text: CoinDetailsViewModel.SecurityLevel.medium.title.uppercased() + ":\n" + CoinDetailsViewModel.SecurityLevel.medium.description),
+                    .text(text: CoinDetailsViewModel.SecurityLevel.low.title.uppercased() + ":\n" + CoinDetailsViewModel.SecurityLevel.low.description),
+                    .header(text: CoinDetailsViewModel.SecurityType.issuance.title),
+                    .text(text: CoinDetailsViewModel.SecurityIssuance.decentralized.title.uppercased() + ":\n" + CoinDetailsViewModel.SecurityIssuance.decentralized.description),
+                    .text(text: CoinDetailsViewModel.SecurityIssuance.centralized.title.uppercased() + ":\n" + CoinDetailsViewModel.SecurityIssuance.centralized.description),
+                    .header(text: CoinDetailsViewModel.SecurityType.confiscationResistance.title),
+                    .text(text: CoinDetailsViewModel.SecurityConfiscationResistance.yes.title.uppercased() + ":\n" + CoinDetailsViewModel.SecurityConfiscationResistance.yes.description),
+                    .text(text: CoinDetailsViewModel.SecurityConfiscationResistance.no.title.uppercased() + ":\n" + CoinDetailsViewModel.SecurityConfiscationResistance.no.description),
+                    .header(text: CoinDetailsViewModel.SecurityType.censorshipResistance.title),
+                    .text(text: CoinDetailsViewModel.SecurityCensorshipResistance.yes.title.uppercased() + ":\n" + CoinDetailsViewModel.SecurityCensorshipResistance.yes.description),
+                    .text(text: CoinDetailsViewModel.SecurityCensorshipResistance.no.title.uppercased() + ":\n" + CoinDetailsViewModel.SecurityCensorshipResistance.no.description)
+                ]
+        )
+    }
+
 }
