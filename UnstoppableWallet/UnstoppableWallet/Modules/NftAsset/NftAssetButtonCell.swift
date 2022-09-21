2eaa36b61c3bd80bf11ca4c4ae7f5cf571c14b1f
ant013 <ant013@mail.ru>
2022-09-21 09:42:06 +0400

    Add send button to NftAssetOverviewViewController.swift, if user has this nft

--- UnstoppableWallet/UnstoppableWallet/Modules/NftAsset/NftAssetButtonCell.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/NftAsset/NftAssetButtonCell.swift
@@ -3,8 +3,12 @@ import ComponentKit
 
 class NftAssetButtonCell: UITableViewCell {
     private let providerButton = PrimaryButton()
+    private let sendButton = PrimaryButton()
     private let moreButton = PrimaryCircleButton()
 
+    private let stackView = UIStackView()
+
+    private var onTapSend: (() -> ())?
     private var onTapProvider: (() -> ())?
     private var onTapMore: (() -> ())?
 
@@ -14,21 +18,26 @@ class NftAssetButtonCell: UITableViewCell {
         backgroundColor = .clear
         selectionStyle = .none
 
-        contentView.addSubview(providerButton)
-        providerButton.snp.makeConstraints { maker in
-            maker.leading.equalToSuperview().inset(CGFloat.margin16)
+        contentView.addSubview(stackView)
+        stackView.snp.makeConstraints { maker in
+            maker.leading.trailing.equalToSuperview().inset(CGFloat.margin16)
             maker.top.equalToSuperview()
+            maker.height.equalTo(CGFloat.heightButton)
         }
+
+        stackView.addArrangedSubview(sendButton)
+        stackView.addArrangedSubview(providerButton)
+        stackView.addArrangedSubview(moreButton)
+
+        stackView.spacing = .margin8
+        stackView.alignment = .fill
+
+        sendButton.set(style: .yellow)
+        sendButton.addTarget(self, action: #selector(onTapSendButton), for: .touchUpInside)
+        sendButton.setTitle("button.send".localized, for: .normal)
 
         providerButton.set(style: .gray)
         providerButton.addTarget(self, action: #selector(onTapProviderButton), for: .touchUpInside)
-
-        contentView.addSubview(moreButton)
-        moreButton.snp.makeConstraints { maker in
-            maker.leading.equalTo(providerButton.snp.trailing).offset(CGFloat.margin8)
-            maker.top.equalToSuperview()
-            maker.trailing.equalToSuperview().inset(CGFloat.margin16)
-        }
 
         moreButton.set(style: .gray)
         moreButton.set(image: UIImage(named: "more_24"))
@@ -39,6 +48,10 @@ class NftAssetButtonCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
+    @objc private func onTapSendButton() {
+        onTapSend?()
+    }
+
     @objc private func onTapProviderButton() {
         onTapProvider?()
     }
@@ -47,9 +60,14 @@ class NftAssetButtonCell: UITableViewCell {
         onTapMore?()
     }
 
-    func bind(providerTitle: String?, onTapProvider: @escaping () -> (), onTapMore: @escaping () -> ()) {
+    func bind(providerTitle: String?, onTapSend: (() -> ())?, onTapProvider: @escaping () -> (), onTapMore: @escaping () -> ()) {
         providerButton.setTitle(providerTitle, for: .normal)
 
+        sendButton.isHidden = onTapSend == nil
+        moreButton.isHidden = onTapSend != nil
+        stackView.distribution = moreButton.isHidden ? .fillEqually : .fill
+
+        self.onTapSend = onTapSend
         self.onTapProvider = onTapProvider
         self.onTapMore = onTapMore
     }
