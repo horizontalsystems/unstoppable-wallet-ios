863b88591ed6211e8672514ad67237f182bd25da
Ermat <ealymbaev@gmail.com>
2023-04-14 18:54:55 +0600

    Fix invalid behavior for clearing accounts when passcode is not set

--- UnstoppableWallet/UnstoppableWallet/Modules/Launch/LaunchModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Launch/LaunchModule.swift
@@ -1,19 +1,35 @@
 import UIKit
+import StorageKit
 
-protocol ILaunchInteractor {
-    var hasAccounts: Bool { get }
-    var passcodeLocked: Bool { get }
-    var isPinSet: Bool { get }
-    var mainShownOnce: Bool { get }
+class LaunchModule {
+
+    static func viewController() -> UIViewController {
+        let service = LaunchService(
+                accountManager: App.shared.accountManager,
+                pinKit: App.shared.pinKit,
+                keychainKit: App.shared.keychainKit,
+                localStorage: App.shared.localStorage
+        )
+
+        switch service.launchMode {
+        case .passcodeNotSet: return NoPasscodeViewController(mode: .noPasscode)
+        case .cannotCheckPasscode: return NoPasscodeViewController(mode: .cannotCheckPasscode)
+        case .intro: return WelcomeScreenViewController()
+        case .unlock: return LockScreenRouter.module(pinKit: App.shared.pinKit, appStart: true)
+        case .main: return MainModule.instance()
+        }
+    }
+
 }
 
-protocol ILaunchPresenter {
-    var launchMode: LaunchMode { get }
-}
+extension LaunchModule {
 
-enum LaunchMode {
-    case noPasscode
-    case intro
-    case unlock
-    case main
+    enum LaunchMode {
+        case passcodeNotSet
+        case cannotCheckPasscode
+        case intro
+        case unlock
+        case main
+    }
+
 }
