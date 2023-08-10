e17b76bdbe457428283ef56552a2d5de8189705d
ant013 <ant013@mail.ru>
2023-08-10 18:10:52 +0600

    Remove deleted funtcions

--- UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Receive/SelectCoin/ReceiveSelectCoinService.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Receive/SelectCoin/ReceiveSelectCoinService.swift
@@ -14,7 +14,45 @@ class ReceiveSelectCoinService {
     }
 
     private func sync() {
-        coins = provider.fetch()
+        let filter = provider.filter
+
+        coins = provider.fetch().sorted { lhsFullCoin, rhsFullCoin in
+            if !filter.isEmpty {
+                let filter = filter.lowercased()
+
+                let lhsExactCode = lhsFullCoin.coin.code.lowercased() == filter
+                let rhsExactCode = rhsFullCoin.coin.code.lowercased() == filter
+
+                if lhsExactCode != rhsExactCode {
+                    return lhsExactCode
+                }
+            }
+
+            let lhsMarketCapRank = lhsFullCoin.coin.marketCapRank ?? Int.max
+            let rhsMarketCapRank = rhsFullCoin.coin.marketCapRank ?? Int.max
+
+            if lhsMarketCapRank != rhsMarketCapRank {
+                return lhsMarketCapRank < rhsMarketCapRank
+            }
+
+            if !filter.isEmpty {
+                let lhsStartsWithCode = lhsFullCoin.coin.code.lowercased().starts(with: filter)
+                let rhsStartsWithCode = rhsFullCoin.coin.code.lowercased().starts(with: filter)
+
+                if lhsStartsWithCode != rhsStartsWithCode {
+                    return lhsStartsWithCode
+                }
+
+                let lhsStartsWithName = lhsFullCoin.coin.name.lowercased().starts(with: filter)
+                let rhsStartsWithName = rhsFullCoin.coin.name.lowercased().starts(with: filter)
+
+                if lhsStartsWithName != rhsStartsWithName {
+                    return lhsStartsWithName
+                }
+            }
+
+            return lhsFullCoin.coin.name.lowercased() < rhsFullCoin.coin.name.lowercased()
+        }
     }
 
 }
