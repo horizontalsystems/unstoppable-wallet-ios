41fb432d03fc023d434fdc59a12592559b94d742
Ermat <ealymbaev@gmail.com>
2023-08-11 12:22:24 +0600

    Remove Coinzix integration and re-enable swap buttons

--- UnstoppableWallet/UnstoppableWallet/Models/Cex.swift
+++ UnstoppableWallet/UnstoppableWallet/Models/Cex.swift
@@ -2,19 +2,16 @@ import UIKit
 
 enum Cex: String, CaseIterable {
     case binance
-    case coinzix
 
     var title: String {
         switch self {
         case .binance: return "Binance"
-        case .coinzix: return "Coinzix"
         }
     }
 
     var url: String {
         switch self {
         case .binance: return "https://www.binance.com"
-        case .coinzix: return "https://coinzix.com"
         }
     }
 
@@ -26,14 +23,12 @@ enum Cex: String, CaseIterable {
     var withdrawalAllowed: Bool {
         switch self {
         case .binance: return false
-        case .coinzix: return true
         }
     }
 
     func restoreViewController(returnViewController: UIViewController?) -> UIViewController {
         switch self {
         case .binance: return RestoreBinanceModule.viewController(returnViewController: returnViewController)
-        case .coinzix: return RestoreCoinzixModule.viewController(returnViewController: returnViewController)
         }
     }
 
