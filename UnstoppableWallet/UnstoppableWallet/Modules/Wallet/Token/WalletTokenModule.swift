4a1b97cd399d24e23c8c4c3d0cb61a162db39061
Ermat <ealymbaev@gmail.com>
2023-08-25 12:07:53 +0600

    Fix displaying txs in WalletToken module when adapters are not ready

--- UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Token/WalletTokenModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Token/WalletTokenModule.swift
@@ -10,22 +10,22 @@ struct WalletTokenModule {
 
         let dataSourceChain = DataSourceChain()
 
-        guard let tokenBalanceDataSource = WalletTokenBalanceModule.dataSource(element: element) else {
-            return nil
-        }
-        dataSourceChain.append(source: tokenBalanceDataSource)
-
-        if let wallet = element.wallet {
-            let transactionsDataSource = TransactionsModule.dataSource(token: wallet.token)
-            dataSourceChain.append(source: transactionsDataSource)
-        }
-
         let viewController = WalletTokenViewController(
                 viewModel: viewModel,
                 dataSource: dataSourceChain
         )
 
+        guard let tokenBalanceDataSource = WalletTokenBalanceModule.dataSource(element: element) else {
+            return nil
+        }
         tokenBalanceDataSource.parentViewController = viewController
+        dataSourceChain.append(source: tokenBalanceDataSource)
+
+        if let wallet = element.wallet {
+            let transactionsDataSource = TransactionsModule.dataSource(token: wallet.token)
+            transactionsDataSource.viewController = viewController
+            dataSourceChain.append(source: transactionsDataSource)
+        }
 
         return viewController
     }
