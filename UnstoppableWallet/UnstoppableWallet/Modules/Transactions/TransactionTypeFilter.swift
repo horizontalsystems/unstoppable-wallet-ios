b870cdf07e32b06d8505c73fe673e1dd367693c6
ant013 <ant013@mail.ru>
2023-09-26 11:24:14 +0400

    Revert "disable swap filter on Transactions module"

--- UnstoppableWallet/UnstoppableWallet/Modules/Transactions/TransactionTypeFilter.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Transactions/TransactionTypeFilter.swift
@@ -1,4 +1,3 @@
 enum TransactionTypeFilter: String, CaseIterable {
-//    case all, incoming, outgoing, swap, approve
-    case all, incoming, outgoing, approve
+    case all, incoming, outgoing, swap, approve
 }
