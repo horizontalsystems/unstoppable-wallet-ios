1aeac9bbceba761074ffb84f58c89319a462b0c6
Ermat <ealymbaev@gmail.com>
2023-10-20 17:44:29 +0600

    Handle click on coins in Widget extension

--- UnstoppableWallet/UnstoppableWallet/Core/Managers/DeepLinkManager.swift
+++ UnstoppableWallet/UnstoppableWallet/Core/Managers/DeepLinkManager.swift
@@ -1,37 +1,47 @@
 import Foundation
-import RxSwift
 import RxRelay
+import RxSwift
 
 class DeepLinkManager {
     private let newSchemeRelay = BehaviorRelay<DeepLink?>(value: nil)
 }
 
 extension DeepLinkManager {
+    var newSchemeObservable: Observable<DeepLink?> {
+        newSchemeRelay.asObservable()
+    }
 
     func handle(url: URL) -> Bool {
-        guard let queryItems = URLComponents(url: url, resolvingAgainstBaseURL: true)?.queryItems, let uri = queryItems.first(where: { $0.name == "uri" })?.value else {
+        guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
             return false
         }
 
-        if url.scheme == "unstoppable.money" || url.scheme == "https" {
-            newSchemeRelay.accept(.walletConnect(url: uri))
+        let scheme = urlComponents.scheme
+        let host = urlComponents.host
+        let path = urlComponents.path
+        let queryItems = urlComponents.queryItems
 
+        if (scheme == "unstoppable.money" && host == "wc") || (scheme == "https" && host == "unstoppable.money" && path == "/wc"),
+           let uri = queryItems?.first(where: { $0.name == "uri" })?.value
+        {
+            newSchemeRelay.accept(.walletConnect(url: uri))
+            return true
+        }
+
+        if scheme == "unstoppable.money" && host == "coin" {
+            let uid = path.replacingOccurrences(of: "/", with: "")
+
+            newSchemeRelay.accept(.coin(uid: uid))
             return true
         }
 
         return false
     }
-
-    var newSchemeObservable: Observable<DeepLink?> {
-        newSchemeRelay.asObservable()
-    }
-
 }
 
 extension DeepLinkManager {
-
     enum DeepLink {
         case walletConnect(url: String)
+        case coin(uid: String)
     }
-
 }
