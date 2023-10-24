2f00a64565832d413584519adbdb779ae9a2d791
Ermat <ealymbaev@gmail.com>
2023-10-24 18:57:21 +0600

    Enable duplicate production build for testers

--- UnstoppableWallet/UnstoppableWallet/Modules/Transactions/TransactionTypeFilter.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Transactions/TransactionTypeFilter.swift
@@ -1,4 +1,7 @@
-enum TransactionTypeFilter: String, CaseIterable {
-//    case all, incoming, outgoing, swap, approve
-    case all, incoming, outgoing, approve
+enum TransactionTypeFilter: String {
+    case all, incoming, outgoing, swap, approve
+
+    static var allCases: [TransactionTypeFilter] {
+        AppConfig.swapEnabled ? [all, incoming, outgoing, swap, approve] : [all, incoming, outgoing, approve]
+    }
 }
