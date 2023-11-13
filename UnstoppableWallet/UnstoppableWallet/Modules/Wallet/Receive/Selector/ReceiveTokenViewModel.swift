10db87fce51cc3fb483f03d1570ffa7194f4e6c6
Ermat <ealymbaev@gmail.com>
2023-11-13 15:17:59 +0600

    Fix blockchain ordering in Receive token list

--- UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Receive/Selector/ReceiveTokenViewModel.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Receive/Selector/ReceiveTokenViewModel.swift
@@ -8,19 +8,19 @@ class ReceiveTokenViewModel: IReceiveSelectorViewModel {
         self.fullCoin = fullCoin
         self.accountType = accountType
     }
-
 }
 
 extension ReceiveTokenViewModel {
-
     var viewItems: [ReceiveSelectorViewModel.ViewItem] {
-        let tokens = fullCoin.tokens.filter { accountType.supports(token: $0) }
+        let tokens = fullCoin.tokens
+            .filter { accountType.supports(token: $0) }
+            .sorted { token, token2 in token.blockchainType.order < token2.blockchainType.order }
+
         return tokens.map {
             ReceiveSelectorViewModel.ViewItem(uid: $0.blockchain.uid,
-                    imageUrl: $0.blockchainType.imageUrl,
-                    title: $0.blockchain.name,
-                    subtitle: $0.blockchainType.description
-            )
+                                              imageUrl: $0.blockchainType.imageUrl,
+                                              title: $0.blockchain.name,
+                                              subtitle: $0.blockchainType.description)
         }
     }
 
