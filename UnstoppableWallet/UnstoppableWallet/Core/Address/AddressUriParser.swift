e49cf0115707a6e998c43a918f4a033af8c605bc
ant013 <ant013@mail.ru>
2023-12-11 17:49:20 +0600

    Handle toncoin deeplink address to open transfer tokens. Add toncoin address to donate screen

--- UnstoppableWallet/UnstoppableWallet/Core/Address/AddressUriParser.swift
+++ UnstoppableWallet/UnstoppableWallet/Core/Address/AddressUriParser.swift
@@ -15,6 +15,38 @@ class AddressUriParser {
         return [prefix, s2].compactMap { $0 }.joined(separator: ":")
     }
 
+    private func parameters(from queryItems: [URLQueryItem]) -> (handled: [AddressUri.Field: String], unhandled: [String: String]) {
+        var handled = [AddressUri.Field: String]()
+        var unhandled = [String: String]()
+
+        for item in queryItems {
+            guard let value = item.value else { continue }
+            if let field = AddressUri.Field(rawValue: item.name) {
+                handled[field] = value
+            } else {
+                unhandled[item.name] = value
+            }
+        }
+
+        return (handled: handled, unhandled: unhandled)
+    }
+
+    private func validate(parameters: [AddressUri.Field: String]) throws {
+        if let blockchainType,
+           let uid: String = parameters[.blockchainUid],
+           blockchainType != BlockchainType(uid: uid)
+        {
+            throw ParseError.invalidBlockchainType
+        }
+
+        if let uid: String = parameters[.tokenUid],
+           let tokenType,
+           tokenType != TokenType(id: uid)
+        {
+            throw ParseError.invalidTokenType
+        }
+    }
+
     private func fullAddress(scheme: String, address: String, uriBlockchainUid: String? = nil) -> String {
         // there is no explicit indication of the blockchain in the uri. We use the rules of the blockchain parser
         guard let uriBlockchainUid else {
@@ -34,45 +66,54 @@ class AddressUriParser {
         return pair(BlockchainType(uid: uriBlockchainUid), address)
     }
 
-    func parse(addressUri: String) -> Result {
-        guard let components = URLComponents(string: addressUri), let scheme = components.scheme else {
-            return .noUri
+    private func handleDeepLink(url: String) -> AddressUri? {
+        guard let components = URLComponents(string: url), let scheme = components.scheme else {
+            return nil
         }
+
+        // try to parse ton deeplink
+        if scheme == DeepLinkManager.tonDeepLinkScheme, let tonScheme = BlockchainType.ton.uriScheme {
+            var uri = AddressUri(scheme: tonScheme)
+            uri.address = components.path.stripping(prefix: "/")
+
+            var params = parameters(from: components.queryItems ?? [])
+            if let amount = params.handled[.amount] {
+                params.handled[.amount] = TonAdapter.amount(kitAmount: amount).description
+            }
+            params.handled[.blockchainUid] = BlockchainType.ton.uid
+
+            uri.parameters = params.handled
+            uri.unhandledParameters = params.unhandled
+
+            return uri
+        }
+
+        return nil
+    }
+
+    func handleAddressUri(uri: String) throws -> AddressUri {
+        guard let components = URLComponents(string: uri), let scheme = components.scheme else {
+            throw ParseError.noUri
+        }
+
         if let validScheme = blockchainType?.uriScheme, components.scheme != validScheme {
-            return .invalidBlockchainType
+            throw ParseError.invalidBlockchainType
         }
 
         var uri = AddressUri(scheme: scheme)
-        guard let parameters = components.queryItems else {
+        guard let items = components.queryItems else {
             uri.address = fullAddress(scheme: scheme, address: components.path)
-            return .uri(uri)
+            return uri
         }
 
-        for parameter in parameters {
-            guard let value = parameter.value else { continue }
-            if let field = AddressUri.Field(rawValue: parameter.name) {
-                uri.parameters[field] = parameter.value
-            } else {
-                uri.unhandledParameters[parameter.name] = value
-            }
-        }
+        let params = parameters(from: items)
+        try validate(parameters: params.handled)
 
-        if let uid: String = uri.value(field: .blockchainUid),
-           let blockchainType,
-           blockchainType != BlockchainType(uid: uid)
-        {
-            return .invalidBlockchainType
-        }
-
-        if let uid: String = uri.value(field: .tokenUid),
-           let tokenType,
-           tokenType != TokenType(id: uid)
-        {
-            return .invalidTokenType
-        }
-
+        uri.parameters = params.handled
+        uri.unhandledParameters = params.unhandled
         uri.address = fullAddress(scheme: scheme, address: components.path, uriBlockchainUid: uri.parameters[.blockchainUid])
-        return .uri(uri)
+
+        return uri
     }
 
     static func hasUriPrefix(text: String) -> Bool {
@@ -81,6 +122,15 @@ class AddressUriParser {
 }
 
 extension AddressUriParser {
+    func parse(url: String) throws -> AddressUri {
+        // check if we try to parse deeplink address (like ton://transfer/<address>)
+        if let addressUri = handleDeepLink(url: url) {
+            return addressUri
+        }
+
+        return try handleAddressUri(uri: url)
+    }
+
     func uri(_ addressUri: AddressUri) -> String {
         var components = URLComponents()
         components.scheme = blockchainType?.uriScheme
@@ -100,12 +150,11 @@ extension AddressUriParser {
 }
 
 extension AddressUriParser {
-    enum Result {
+    enum ParseError: Error {
         case wrongUri
         case invalidBlockchainType
         case invalidTokenType
         case noUri
-        case uri(AddressUri)
     }
 }
 
