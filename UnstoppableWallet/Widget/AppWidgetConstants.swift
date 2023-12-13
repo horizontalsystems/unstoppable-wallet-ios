9768e3d830a25473fad0f2594544a918b92d2218
ant013 <ant013@mail.ru>
2023-12-13 14:53:38 +0600

    Update widgets on willEnterForeground

--- UnstoppableWallet/Widget/AppWidgetConstants.swift
+++ UnstoppableWallet/Widget/AppWidgetConstants.swift
@@ -1,4 +1,6 @@
 enum AppWidgetConstants {
+    static let allKinds = [singleCoinPriceWidgetKind, topCoinsWidgetKind, watchlistWidgetKind]
+
     static let singleCoinPriceWidgetKind: String = "io.horizontalsystems.unstoppable.SingleCoinPriceWidget"
     static let topCoinsWidgetKind: String = "io.horizontalsystems.unstoppable.TopCoinsWidget"
     static let watchlistWidgetKind: String = "io.horizontalsystems.unstoppable.WatchlistWidget"
