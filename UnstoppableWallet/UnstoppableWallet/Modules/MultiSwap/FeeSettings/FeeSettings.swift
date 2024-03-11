30a37c825e916ffaa3fef910543f7ed7df161ac1
Esenbek <esenbekkd@gmail.com>
2024-03-11 16:50:12 +0600

    Minor improvements to fee settings

--- UnstoppableWallet/UnstoppableWallet/Modules/MultiSwap/FeeSettings/FeeSettings.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/MultiSwap/FeeSettings/FeeSettings.swift
@@ -3,13 +3,13 @@ import Foundation
 import MarketKit
 
 enum FeeSettings {
-    static func feeAmount(feeToken: Token, currency: Currency, feeTokenRate: Decimal?, loading: Bool, gasLimit: Int?, gasPrice: GasPrice?) -> (String?, String?) {
+    static func feeAmount(feeToken: Token, currency: Currency, feeTokenRate: Decimal?, loading: Bool, gasLimit: Int?, gasPrice: GasPrice?) -> (String?, String?, String?) {
         guard !loading else {
-            return (nil, nil)
+            return (nil, nil, nil)
         }
 
         guard let gasLimit, let gasPrice else {
-            return ("n/a".localized, "n/a".localized)
+            return ("n/a".localized, "n/a".localized, "n/a".localized)
         }
 
         let amount = Decimal(gasLimit) * Decimal(gasPrice.max) / pow(10, feeToken.decimals)
@@ -17,13 +17,13 @@ enum FeeSettings {
         let coinValueString = ValueFormatter.instance.formatShort(coinValue: coinValue)
 
         guard let feeTokenRate else {
-            return (coinValueString, "n/a".localized)
+            return (coinValueString, "n/a".localized, gasLimit.description)
         }
 
         let currencyValue = CurrencyValue(currency: currency, value: amount * feeTokenRate)
         let currencyValueString = ValueFormatter.instance.formatShort(currencyValue: currencyValue)
 
-        return (coinValueString, currencyValueString)
+        return (coinValueString, currencyValueString, gasLimit.description)
     }
 
     struct ViewItem {
