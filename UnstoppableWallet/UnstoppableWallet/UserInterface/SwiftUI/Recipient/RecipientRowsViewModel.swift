0f134e7da23748c43c69e1a924f4424e3619fbf1
ant013 <ant013@mail.ru>
2024-03-18 15:34:50 +0600

    Add evm labels for swap confirmation

--- UnstoppableWallet/UnstoppableWallet/UserInterface/SwiftUI/Recipient/RecipientRowsViewModel.swift
+++ UnstoppableWallet/UnstoppableWallet/UserInterface/SwiftUI/Recipient/RecipientRowsViewModel.swift
@@ -4,6 +4,7 @@ import RxSwift
 
 class RecipientRowsViewModel: ObservableObject {
     let disposeBag = DisposeBag()
+    let evmLabelManager = App.shared.evmLabelManager
     let manager = App.shared.contactManager
 
     let address: String
@@ -20,6 +21,10 @@ class RecipientRowsViewModel: ObservableObject {
     }
 
     private func sync() {
+        if let evmLabel = evmLabelManager.addressLabel(address: address) {
+            name = evmLabel
+            return
+        }
         name = manager.all?.by(address: address, blockchainUid: blockchainType.uid)?.name
     }
 
