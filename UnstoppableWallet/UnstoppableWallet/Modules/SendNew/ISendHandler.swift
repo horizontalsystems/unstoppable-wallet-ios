729e72519d75ff9ac201fb04db20155565141871
Esenbek <esenbekkd@gmail.com>
2024-05-06 12:26:41 +0600

    Pass initial transaction settings to EvmTransactionService

--- UnstoppableWallet/UnstoppableWallet/Modules/SendNew/ISendHandler.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/SendNew/ISendHandler.swift
@@ -4,6 +4,13 @@ protocol ISendHandler {
     var baseToken: Token { get }
     var syncingText: String? { get }
     var expirationDuration: Int? { get }
+    var initialTransactionSettings: InitialTransactionSettings? { get }
     func sendData(transactionSettings: TransactionSettings?) async throws -> ISendData
     func send(data: ISendData) async throws
+}
+
+extension ISendHandler {
+    var initialTransactionSettings: InitialTransactionSettings? {
+        nil
+    }
 }
