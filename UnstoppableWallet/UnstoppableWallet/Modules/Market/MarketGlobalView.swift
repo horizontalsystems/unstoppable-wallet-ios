0229b774d339a14120d96e204cd858b6e826f1a4
EA <ealymbaev@gmail.com>
2024-05-15 15:30:51 +0600

    Apply new design for market global data

--- UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketGlobalView.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Market/MarketGlobalView.swift
@@ -7,57 +7,55 @@ struct MarketGlobalView: View {
     @State private var presentedGlobalMarketMetricsType: MarketGlobalModule.MetricsType?
 
     var body: some View {
-        ZStack {
-            HStack(spacing: 0) {
-                itemView(title: "market.global.market_cap".localized, item: viewModel.globalMarketData?.marketCap, metricsType: .totalMarketCap)
-                divider()
-                itemView(title: "market.global.volume".localized, item: viewModel.globalMarketData?.volume, metricsType: .volume24h)
-                divider()
-                itemView(title: "market.global.defi_cap".localized, item: viewModel.globalMarketData?.defiCap, metricsType: .defiCap)
-                divider()
-                itemView(title: "market.global.defi_in_tvl".localized, item: viewModel.globalMarketData?.tvlInDefi, metricsType: .tvlInDefi)
+        VStack(spacing: 0) {
+            if let globalMarketData = viewModel.globalMarketData {
+                MarqueeView(targetVelocity: 30) {
+                    content(globalMarketData: globalMarketData)
+                }
+            } else {
+                ZStack {
+                    HStack(spacing: .margin8) {
+                        content(globalMarketData: nil)
+                    }
+                    .padding(.leading, .margin8)
+                    .fixedSize()
+                }
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
             }
-            .padding(.horizontal, .margin4)
-            .modifier(ThemeListStyleModifier())
-            .animation(.default, value: viewModel.globalMarketData == nil)
+
+            Rectangle()
+                .fill(Color.themeSteel10)
+                .frame(maxWidth: .infinity)
+                .frame(height: 1)
         }
-        .padding(.horizontal, .margin16)
-        .padding(.top, .margin12)
-        .padding(.bottom, .margin16)
+        .animation(.default, value: viewModel.globalMarketData == nil)
         .sheet(item: $presentedGlobalMarketMetricsType) { metricsType in
             MarketGlobalMetricsView(metricsType: metricsType).ignoresSafeArea()
         }
     }
 
+    @ViewBuilder private func content(globalMarketData: MarketGlobalViewModel.GlobalMarketData?) -> some View {
+        itemView(title: "market.global.market_cap".localized, item: globalMarketData?.marketCap, metricsType: .totalMarketCap)
+        itemView(title: "market.global.volume".localized, item: globalMarketData?.volume, metricsType: .volume24h)
+        itemView(title: "market.global.defi_cap".localized, item: globalMarketData?.defiCap, metricsType: .defiCap)
+        itemView(title: "market.global.defi_in_tvl".localized, item: globalMarketData?.tvlInDefi, metricsType: .tvlInDefi)
+    }
+
     @ViewBuilder private func itemView(title: String, item: MarketGlobalViewModel.GlobalMarketItem?, metricsType: MarketGlobalModule.MetricsType) -> some View {
-        VStack(alignment: .leading, spacing: .margin8) {
-            Text(title)
-                .textCaption()
-                .lineLimit(1)
-                .truncationMode(.middle)
+        HStack(spacing: .margin4) {
+            Text(title).textCaption()
 
-            VStack(alignment: .leading, spacing: 1) {
-                Text(item?.amount ?? "$2.34T")
-                    .textCaption(color: .themeBran)
-                    .lineLimit(1)
-                    .truncationMode(.middle)
-                    .redacted(value: item?.amount)
+            Text(item?.amount ?? "$2.34T")
+                .textCaption(color: .themeBran)
+                .redacted(value: item?.amount)
 
-                DiffText(item?.diff, font: .themeCaption)
-                    .redacted(value: item?.diff)
-            }
+            DiffText(item?.diff, font: .themeCaption)
+                .redacted(value: item?.diff)
         }
         .padding(.horizontal, .margin8)
-        .padding(.vertical, .margin12)
-        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.vertical, .margin16)
         .onTapGesture {
             presentedGlobalMarketMetricsType = metricsType
         }
-    }
-
-    @ViewBuilder private func divider() -> some View {
-        Rectangle()
-            .fill(Color.themeSteel20)
-            .frame(width: .heightOneDp, height: 50)
     }
 }
