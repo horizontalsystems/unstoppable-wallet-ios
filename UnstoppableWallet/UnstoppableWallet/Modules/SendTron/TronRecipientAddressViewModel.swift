5a5cc45bcb12e64b459094081f81d1e3b0748237
Esenbek <esenbekkd@gmail.com>
2024-05-24 15:10:29 +0600

    Remove 'This address is not active' warning for TRON

--- UnstoppableWallet/UnstoppableWallet/Modules/SendTron/TronRecipientAddressViewModel.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/SendTron/TronRecipientAddressViewModel.swift
@@ -1,22 +1,11 @@
 import RxSwift
 
 class TronRecipientAddressViewModel: RecipientAddressViewModel {
-    private let disposeBag = DisposeBag()
     private let sendService: SendTronService
 
     init(service: AddressService, handlerDelegate: IRecipientAddressService?, sendService: SendTronService) {
         self.sendService = sendService
         super.init(service: service, handlerDelegate: handlerDelegate)
-
-        subscribe(disposeBag, sendService.activeAddressObservable) { [weak self] in self?.handle(active: $0) }
-    }
-
-    private func handle(active: Bool) {
-        if active {
-            cautionRelay.accept(nil)
-        } else {
-            cautionRelay.accept(Caution(text: "tron.send.inactive_address".localized, type: .warning))
-        }
     }
 
     override func sync(state: AddressService.State? = nil, customError: Error? = nil) {
