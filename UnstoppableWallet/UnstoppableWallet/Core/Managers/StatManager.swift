c408ba82c25aee91f5214f5b6a0f352397576121
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2024-05-27 13:03:50 +0400

    Disable any statistic logs

--- UnstoppableWallet/UnstoppableWallet/Core/Managers/StatManager.swift
+++ UnstoppableWallet/UnstoppableWallet/Core/Managers/StatManager.swift
@@ -28,73 +28,73 @@ class StatManager {
     }
 
     func logStat(eventPage: StatPage, eventSection: StatSection? = nil, event: StatEvent) {
-        var parameters: [String: Any]?
-
-        if let params = event.params {
-            parameters = [String: Any]()
-
-            for (key, value) in params {
-                parameters?[key.rawValue] = value
-            }
-        }
-
-        let record = StatRecord(
-            timestamp: Int(Date().timeIntervalSince1970),
-            eventPage: eventPage.rawValue,
-            eventSection: eventSection?.rawValue,
-            event: event.name,
-            params: parameters
-        )
-
-        do {
-            try storage.save(record: record)
-        } catch {
-            print("Cannot save StatRecord: \(error)")
-        }
+//        var parameters: [String: Any]?
+//
+//        if let params = event.params {
+//            parameters = [String: Any]()
+//
+//            for (key, value) in params {
+//                parameters?[key.rawValue] = value
+//            }
+//        }
+//
+//        let record = StatRecord(
+//            timestamp: Int(Date().timeIntervalSince1970),
+//            eventPage: eventPage.rawValue,
+//            eventSection: eventSection?.rawValue,
+//            event: event.name,
+//            params: parameters
+//        )
+//
+//        do {
+//            try storage.save(record: record)
+//        } catch {
+//            print("Cannot save StatRecord: \(error)")
+//        }
     }
 
     func sendStats() {
-        let lastSent: Double? = userDefaultsStorage.value(for: Self.keyLastSent)
-
-        if let lastSent, Date().timeIntervalSince1970 - lastSent < Self.sendThreshold {
-            return
-        }
-
-        Task { [storage] in
-            let records = try storage.all()
-
-            guard !records.isEmpty else {
-                return
-            }
-
-            let stats = records.map { record in
-                var object: [String: Any] = [
-                    "time": record.timestamp,
-                    "event_page": record.eventPage,
-                    "event": record.event,
-                ]
-
-                if let eventSection = record.eventSection {
-                    object["event_section"] = eventSection
-                }
-
-                if let params = record.params {
-                    for (key, value) in params {
-                        object[key] = value
-                    }
-                }
-
-                return object
-            }
-
-//            let data = try JSONSerialization.data(withJSONObject: stats)
-//            let string = String(data: data, encoding: .utf8)
-//            print(string ?? "N/A")
-
-            try await marketKit.send(stats: stats, appVersion: appVersion, appId: appId)
-
-            userDefaultsStorage.set(value: Date().timeIntervalSince1970, for: Self.keyLastSent)
-            try storage.clear()
-        }
+//        let lastSent: Double? = userDefaultsStorage.value(for: Self.keyLastSent)
+//
+//        if let lastSent, Date().timeIntervalSince1970 - lastSent < Self.sendThreshold {
+//            return
+//        }
+//
+//        Task { [storage] in
+//            let records = try storage.all()
+//
+//            guard !records.isEmpty else {
+//                return
+//            }
+//
+//            let stats = records.map { record in
+//                var object: [String: Any] = [
+//                    "time": record.timestamp,
+//                    "event_page": record.eventPage,
+//                    "event": record.event,
+//                ]
+//
+//                if let eventSection = record.eventSection {
+//                    object["event_section"] = eventSection
+//                }
+//
+//                if let params = record.params {
+//                    for (key, value) in params {
+//                        object[key] = value
+//                    }
+//                }
+//
+//                return object
+//            }
+//
+////            let data = try JSONSerialization.data(withJSONObject: stats)
+////            let string = String(data: data, encoding: .utf8)
+////            print(string ?? "N/A")
+//
+//            try await marketKit.send(stats: stats, appVersion: appVersion, appId: appId)
+//
+//            userDefaultsStorage.set(value: Date().timeIntervalSince1970, for: Self.keyLastSent)
+//            try storage.clear()
+//        }
     }
 }
