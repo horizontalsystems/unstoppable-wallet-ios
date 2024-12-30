457c4e1803549b53260884707255ce5dae21e5bb
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2024-12-30 12:23:25 +0700

    Add premium UI for advanced search

---
+++ UnstoppableWallet/UnstoppableWallet/UserInterface/SwiftUI/PremiumListSectionHeader.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct PremiumListSectionHeader: View {
+    var body: some View {
+        HStack(spacing: .margin6) {
+            Image("crown_16")
+                .renderingMode(.template)
+                .foregroundColor(.themeJacob)
+            
+            Text("subscription.premium.label".localized)
+                .themeSubhead1(color: .themeJacob)
+        }
+        .padding(EdgeInsets(top: .margin6, leading: .margin16, bottom: 0, trailing: .margin16))
+        .frame(height: .margin32, alignment: .top)
+    }
+}
