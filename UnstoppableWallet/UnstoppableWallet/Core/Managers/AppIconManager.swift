3bbe967fd2709aabb6f5a0563c10b2c7ebeda578
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2025-01-24 11:34:15 +0600

    Add new appIcons

--- UnstoppableWallet/UnstoppableWallet/Core/Managers/AppIconManager.swift
+++ UnstoppableWallet/UnstoppableWallet/Core/Managers/AppIconManager.swift
@@ -13,8 +13,12 @@ class AppIconManager {
         .alternate(name: "AppIconPunk", imageName: "app_icon_punk", title: "Punk"),
         .alternate(name: "AppIcon1874", imageName: "app_icon_1874", title: "#1874"),
         .alternate(name: "AppIcon8ball", imageName: "app_icon_8ball", title: "8ball"),
+        .alternate(name: "AppIconPepe", imageName: "app_icon_pepe", title: "Pepe"),
         .alternate(name: "AppIconIvfun", imageName: "app_icon_ivfun", title: "Ivfun"),
-        .alternate(name: "AppIconDuck", imageName: "app_icon_duck", title: "Duck"),
+        .alternate(name: "AppIconDoge", imageName: "app_icon_doge", title: "Doge"),
+        .alternate(name: "AppIconGigaChad", imageName: "app_icon_giga_chad", title: "Gigachad"),
+        .alternate(name: "AppIconPlFlag", imageName: "app_icon_pl_flag", title: "PL"),
+        .alternate(name: "AppIconYesChad", imageName: "app_icon_yes_chad", title: "Yeschad"),
     ]
 
     private let appIconRelay = PublishRelay<AppIcon>()
