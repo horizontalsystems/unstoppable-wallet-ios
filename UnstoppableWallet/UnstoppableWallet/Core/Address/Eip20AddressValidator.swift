c0af5be7d2286c9f3014c16d2f2284368a905bac
Esenbek <esenbekkd@gmail.com>
2025-02-13 12:20:34 +0600

    Add USDC and PYUSD blacklist check over RPC

--- UnstoppableWallet/UnstoppableWallet/Core/Address/Eip20AddressValidator.swift
+++ UnstoppableWallet/UnstoppableWallet/Core/Address/Eip20AddressValidator.swift
@@ -1,6 +1,7 @@
 import EvmKit
 import HsToolKit
 import MarketKit
+import Foundation
 
 class Eip20AddressValidator {
     private let evmSyncSourceManager: EvmSyncSourceManager
@@ -14,8 +15,16 @@ class Eip20AddressValidator {
             return nil
         }
 
-        if Eip20AddressValidator.IsBlacklistedMethod.contractAddresses.contains(contractAddress.eip55) {
-            return IsBlacklistedMethod(address: evmAddress)
+        if Eip20AddressValidator.IsBlacklistedMethodUSDT.contractAddresses.contains(contractAddress.eip55) {
+            return IsBlacklistedMethodUSDT(address: evmAddress)
+        }
+
+        if Eip20AddressValidator.IsBlacklistedMethodUSDC.contractAddresses.contains(contractAddress.eip55) {
+            return IsBlacklistedMethodUSDC(address: evmAddress)
+        }
+
+        if Eip20AddressValidator.IsFrozenMethodPYUSD.contractAddresses.contains(contractAddress.eip55) {
+            return IsFrozenMethodPYUSD(address: evmAddress)
         }
 
         return nil
@@ -50,12 +59,13 @@ extension Eip20AddressValidator: IAddressSecurityChecker {
             data: method.encodedABI(),
             defaultBlockParameter: .latest
         )
+
         return responseData.contains(0x01)
     }
 }
 
 extension Eip20AddressValidator {
-    class IsBlacklistedMethod: ContractMethod {
+    class IsBlacklistedMethodUSDT: ContractMethod {
         static let contractAddresses = [
             "0xdAC17F958D2ee523a2206206994597C13D831ec7",
         ]
@@ -68,6 +78,46 @@ extension Eip20AddressValidator {
 
         override var methodSignature: String {
             "isBlackListed(address)"
+        }
+
+        override var arguments: [Any] {
+            [address]
+        }
+    }
+
+    class IsBlacklistedMethodUSDC: ContractMethod {
+        static let contractAddresses = [
+            "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+        ]
+
+        private let address: EvmKit.Address
+
+        init(address: EvmKit.Address) {
+            self.address = address
+        }
+
+        override var methodSignature: String {
+            "isBlacklisted(address)"
+        }
+
+        override var arguments: [Any] {
+            [address]
+        }
+    }
+
+    class IsFrozenMethodPYUSD: ContractMethod {
+        static let contractAddresses = [
+            "0x6c3ea9036406852006290770BEdFcAbA0e23A0e8"
+        ]
+
+        private let address: EvmKit.Address
+
+        init(address: EvmKit.Address) {
+            self.address = address
+        }
+
+        override var methodSignature: String {
+            "isFrozen(address)"
         }
 
         override var arguments: [Any] {
