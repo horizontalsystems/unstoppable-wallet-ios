d63fae07d423fe870b5fce57daa45f0c099f5266
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2025-07-14 16:13:53 +0600

    Refactor deeplink managers and wallet connect verification

---
+++ UnstoppableWallet/UnstoppableWallet/Modules/WalletConnect/WalletConnectVerificationModel.swift
@@ -0,0 +1,90 @@
+import Combine
+import Foundation
+
+class WalletConnectVerificationModel: ObservableObject {
+    private let accountManager: AccountManager
+    private let cloudBackupManager: CloudBackupManager
+
+    init(accountManager: AccountManager, cloudBackupManager: CloudBackupManager) {
+        self.accountManager = accountManager
+        self.cloudBackupManager = cloudBackupManager
+    }
+
+    func handle(onSuccess: () -> Void) {
+        guard let activeAccount = accountManager.activeAccount else {
+            presentNoAccount()
+            return
+        }
+
+        if !activeAccount.type.supportsWalletConnect {
+            presentNotSupported(accountType: activeAccount.type) { [weak self] in
+                self?.presentSwitchAccount()
+            }
+            return
+        }
+
+        if !activeAccount.backedUp, !cloudBackupManager.backedUp(uniqueId: activeAccount.type.uniqueId()) {
+            Coordinator.shared.present(type: .bottomSheet) { isPresented in
+                BackupRequiredView.prompt(
+                    account: activeAccount,
+                    description: "wallet_connect.unbackuped_account.description".localized(activeAccount.name),
+                    isPresented: isPresented
+                )
+            }
+
+            stat(page: .walletConnect, event: .open(page: .backupRequired))
+            return
+        }
+
+        onSuccess()
+    }
+
+    private func presentSwitchAccount() {
+        Coordinator.shared.present { _ in
+            SwitchAccountView()
+        }
+    }
+
+    private func presentNoAccount() {
+        Coordinator.shared.present(type: .bottomSheet) { isPresented in
+            BottomSheetView(
+                icon: .local(name: "wallet_connect_24", tint: .themeJacob),
+                title: "wallet_connect.title".localized,
+                items: [
+                    .highlightedDescription(text: "wallet_connect.no_account.description".localized),
+                ],
+                buttons: [
+                    .init(style: .yellow, title: "button.ok".localized) {
+                        isPresented.wrappedValue = false
+                    },
+                ],
+                isPresented: isPresented
+            )
+        }
+    }
+
+    private func presentNotSupported(accountType: AccountType, onSwitch: (() -> Void)?) {
+        Coordinator.shared.present(type: .bottomSheet) { isPresented in
+            BottomSheetView(
+                icon: .local(name: "wallet_connect_24", tint: .themeJacob),
+                title: "wallet_connect.title".localized,
+                items: [
+                    .highlightedDescription(text: "wallet_connect.non_supported_account.description".localized(accountType.description)),
+                ],
+                buttons: [
+                    .init(style: .yellow, title: "wallet_connect.non_supported_account.switch".localized) {
+                        isPresented.wrappedValue = false
+
+                        DispatchQueue.main.async {
+                            onSwitch?()
+                        }
+                    },
+                    .init(style: .transparent, title: "button.cancel".localized) {
+                        isPresented.wrappedValue = false
+                    },
+                ],
+                isPresented: isPresented
+            )
+        }
+    }
+}
