2bf449e6d65f422dc5b6e5e429b77df454ec7760
EA <ealymbaev@gmail.com>
2025-08-19 13:09:12 +0600

    Fix passcode validation when duress passcode is set

--- UnstoppableWallet/UnstoppableWallet/Modules/Passcode/Unlock/AppUnlockViewModel.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Passcode/Unlock/AppUnlockViewModel.swift
@@ -3,6 +3,10 @@ import Combine
 class AppUnlockViewModel: BaseUnlockViewModel {
     private let lockManager = Core.shared.lockManager
 
+    override func isValid(passcode: String) -> Bool {
+        passcodeManager.has(passcode: passcode)
+    }
+
     override func onEnterValid(passcode: String) {
         passcodeManager.set(currentPasscode: passcode)
         lockManager.unlock()
