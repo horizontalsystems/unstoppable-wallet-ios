1f508ee917061f05c698525652cc9a7fbeeecb52
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2025-09-18 18:32:25 +0600

    Update appIcons

--- UnstoppableWallet/UnstoppableWallet/Core/Managers/AppIconManager.swift
+++ UnstoppableWallet/UnstoppableWallet/Core/Managers/AppIconManager.swift
@@ -7,17 +7,17 @@ class AppIconManager {
         .main,
         .alternate(name: "AppIconDark", imageName: "app_icon_dark", title: "Dark"),
         .alternate(name: "AppIconMono", imageName: "app_icon_mono", title: "Mono"),
-        .alternate(name: "AppIconLeo", imageName: "app_icon_leo", title: "Leo"),
-        .alternate(name: "AppIconMustang", imageName: "app_icon_mustang", title: "Mustang"),
-        .alternate(name: "AppIconYak", imageName: "app_icon_yak", title: "Yak"),
-        .alternate(name: "AppIconPunk", imageName: "app_icon_punk", title: "Punk"),
-        .alternate(name: "AppIcon1874", imageName: "app_icon_1874", title: "#1874"),
         .alternate(name: "AppIcon8ball", imageName: "app_icon_8ball", title: "8ball"),
+        .alternate(name: "AppIconMonero", imageName: "app_icon_monero", title: "Monero"),
+        .alternate(name: "AppIconZcash", imageName: "app_icon_zcash", title: "Zcash"),
         .alternate(name: "AppIconPepe", imageName: "app_icon_pepe", title: "Pepe"),
         .alternate(name: "AppIconDoge", imageName: "app_icon_doge", title: "Doge"),
-        .alternate(name: "AppIconGigaChad", imageName: "app_icon_giga_chad", title: "Gigachad"),
+        .alternate(name: "AppIconPunk", imageName: "app_icon_punk", title: "Punk"),
+        .alternate(name: "AppIcon1874", imageName: "app_icon_1874", title: "#1874"),
         .alternate(name: "AppIconPlFlag", imageName: "app_icon_pl_flag", title: "PL"),
+        .alternate(name: "AppIconYS", imageName: "app_icon_ys", title: "Sinwar"),
         .alternate(name: "AppIconYesChad", imageName: "app_icon_yes_chad", title: "Yeschad"),
+        .alternate(name: "AppIconGigaChad", imageName: "app_icon_giga_chad", title: "Gigachad"),
     ]
 
     private let appIconRelay = PublishRelay<AppIcon>()
