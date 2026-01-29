b5bcf358eaa77295baa528055408c42591bd68b5
EA <ealymbaev@gmail.com>
2026-01-29 13:11:27 +0600

    Update cover screen

--- UnstoppableWallet/UnstoppableWallet/Modules/Main/CoverView.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Main/CoverView.swift
@@ -2,18 +2,8 @@ import SwiftUI
 
 struct CoverView: View {
     var body: some View {
-        ThemeView {
-            VStack(spacing: .margin24) {
-                Image(AppIconManager.currentAppIcon.imageName)
-                    .resizable()
-                    .scaledToFit()
-                    .clipShape(RoundedRectangle(cornerRadius: .cornerRadius12, style: .continuous))
-                    .frame(width: 60, height: 60)
-
-                Text(AppConfig.appName)
-                    .font(.system(size: 34, weight: .bold))
-                    .multilineTextAlignment(.center)
-            }
+        ThemeRadialView {
+            Image("Intro - Stay Private")
         }
     }
 }
