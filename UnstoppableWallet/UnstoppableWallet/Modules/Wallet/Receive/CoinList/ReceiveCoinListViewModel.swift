3779ffd558c97bfa4ced625f9ab9da58054da14c
EA <ealymbaev@gmail.com>
2026-01-29 15:38:29 +0600

    Update birthday height behavior when activated from receive

--- UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Receive/CoinList/ReceiveCoinListViewModel.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Receive/CoinList/ReceiveCoinListViewModel.swift
@@ -8,8 +8,6 @@ class ReceiveCoinListViewModel: ObservableObject {
     private var cancellables = Set<AnyCancellable>()
 
     @Published var viewItems = [ViewItem]()
-    @Published var enableTokenWithBirthday: Token?
-    @Published var pushCoinUid: String?
     @Published var searchText: String = ""
 
     init(account: Account, service: ReceiveCoinListService) {
@@ -47,33 +45,12 @@ extension ReceiveCoinListViewModel {
         }
     }
 
-    func handleAfterEnable(uid: String, _ block: (FullCoin) -> Void) {
-        guard let fullCoin = service.fullCoin(uid: uid) else {
-            return
-        }
-
-        let eligibleTokens = fullCoin.tokens.filter { account.type.supports(token: $0) }
-
-        // Check if need to enable token with Settings
-        switch eligibleTokens.first?.blockchainType {
-        case .zcash, .monero:
-            if let token = eligibleTokens.first, !ReceiveModule.isEnabled(token: token) {
-                enableTokenWithBirthday = eligibleTokens.first
-                return
-            }
-        default: ()
-        }
-
-        block(fullCoin)
-    }
-
-    func createWallet(token: Token, height: Int?) {
-        service.onRestoreWithBirthdayHeight(account: account, token: token, height: height)
-        pushCoinUid = token.coin.uid
-    }
-
     func fullCoin(uid: String) -> FullCoin? {
         service.fullCoin(uid: uid)
+    }
+
+    func prepareEnable(fullCoin: FullCoin) {
+        service.prepareEnable(fullCoin: fullCoin, account: account)
     }
 }
 
