c18d1c82ac3f735c998b4e18e0b447430450e832
EA <ealymbaev@gmail.com>
2026-03-24 15:35:26 +0600

    Refactor BtcBlockchainSettings module

--- UnstoppableWallet/UnstoppableWallet/Modules/BtcBlockchainSettings/BtcBlockchainSettingsView.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/BtcBlockchainSettings/BtcBlockchainSettingsView.swift
@@ -1,77 +1,87 @@
 import Kingfisher
+import MarketKit
 import SwiftUI
 
 struct BtcBlockchainSettingsView: View {
-    @ObservedObject var viewModel: BtcBlockchainSettingsViewModel
+    @StateObject private var viewModel: BtcBlockchainSettingsViewModel
+    @Binding private var isPresented: Bool
 
-    @Environment(\.presentationMode) private var presentationMode
+    init(blockchain: Blockchain, isPresented: Binding<Bool>) {
+        _viewModel = .init(wrappedValue: BtcBlockchainSettingsViewModel(blockchain: blockchain))
+        _isPresented = isPresented
+    }
 
     var body: some View {
-        ThemeView {
-            BottomGradientWrapper {
-                ScrollView {
-                    VStack(spacing: .margin32) {
-                        VStack(spacing: .margin32) {
-                            Text("btc_blockchain_settings.restore_source.description".localized)
-                                .themeSubhead2()
-                                .padding(EdgeInsets(top: 0, leading: .margin16, bottom: 0, trailing: .margin16))
+        ThemeNavigationStack {
+            ThemeView {
+                BottomGradientWrapper {
+                    ScrollView {
+                        VStack(spacing: 32) {
+                            ThemeText("btc_blockchain_settings.restore_source.description".localized, style: .subhead)
+                                .padding(.horizontal, 16)
 
                             ListSection {
-                                ForEach(viewModel.restoreModes, id: \.restoreMode.id) { restoreMode in
-                                    ClickableRow(action: {
-                                        viewModel.selectedRestoreMode = restoreMode.restoreMode
-                                    }) {
-                                        switch restoreMode.icon {
-                                        case let .local(name):
-                                            Image(name)
-                                        case let .remote(url):
-                                            KFImage.url(URL(string: url))
-                                                .resizable()
-                                                .frame(width: .iconSize32, height: .iconSize32)
+                                ForEach(viewModel.restoreModes) { restoreMode in
+                                    Cell(
+                                        left: {
+                                            switch restoreMode {
+                                            case .blockchair:
+                                                Image("blockchair_32")
+                                            case .hybrid:
+                                                Image("api_placeholder_32")
+                                            case .blockchain:
+                                                KFImage.url(URL(string: viewModel.blockchain.type.imageUrl))
+                                                    .resizable()
+                                                    .frame(size: 32)
+                                            }
+                                        },
+                                        middle: {
+                                            MultiText(
+                                                title: restoreMode.title(blockchain: viewModel.blockchain),
+                                                subtitle: restoreMode.description
+                                            )
+                                        },
+                                        right: {
+                                            if restoreMode == viewModel.selectedRestoreMode {
+                                                Image.checkIcon
+                                            }
+                                        },
+                                        action: {
+                                            viewModel.selectedRestoreMode = restoreMode
                                         }
-
-                                        VStack(spacing: 1) {
-                                            Text(restoreMode.title).themeBody()
-                                            Text(restoreMode.description).themeSubhead2()
-                                        }
-
-                                        if restoreMode.restoreMode == viewModel.selectedRestoreMode {
-                                            Image.checkIcon
-                                        }
-                                    }
+                                    )
                                 }
                             }
+
+                            HighlightedTextView(text: "btc_blockchain_settings.restore_source.alert".localized(viewModel.blockchain.name))
                         }
-
-                        HighlightedTextView(text: "btc_blockchain_settings.restore_source.alert".localized(viewModel.title))
+                        .padding(EdgeInsets(top: 12, leading: 16, bottom: 32, trailing: 16))
                     }
-                    .padding(EdgeInsets(top: .margin12, leading: .margin16, bottom: .margin32, trailing: .margin16))
+                } bottomContent: {
+                    Button(action: {
+                        viewModel.save()
+                        isPresented = false
+                    }) {
+                        Text("button.save".localized)
+                    }
+                    .buttonStyle(PrimaryButtonStyle(style: .yellow))
+                    .disabled(!viewModel.saveEnabled)
                 }
-            } bottomContent: {
-                Button(action: {
-                    viewModel.onTapSave()
-                    presentationMode.wrappedValue.dismiss()
-                }) {
-                    Text("button.save".localized)
+            }
+            .navigationTitle(viewModel.blockchain.name)
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    KFImage.url(URL(string: viewModel.blockchain.type.imageUrl))
+                        .resizable()
+                        .frame(width: .iconSize24, height: .iconSize24)
                 }
-                .buttonStyle(PrimaryButtonStyle(style: .yellow))
-                .disabled(!viewModel.saveEnabled)
-            }
-        }
-        .navigationTitle(viewModel.title)
-        .navigationBarTitleDisplayMode(.large)
-        .toolbar {
-            ToolbarItem(placement: .primaryAction) {
-                KFImage.url(URL(string: viewModel.iconUrl))
-                    .resizable()
-                    .frame(width: .iconSize24, height: .iconSize24)
-            }
 
-            ToolbarItem(placement: .cancellationAction) {
-                Button(action: {
-                    presentationMode.wrappedValue.dismiss()
-                }) {
-                    Image("close")
+                ToolbarItem(placement: .cancellationAction) {
+                    Button(action: {
+                        isPresented = false
+                    }) {
+                        Image("close")
+                    }
                 }
             }
         }
