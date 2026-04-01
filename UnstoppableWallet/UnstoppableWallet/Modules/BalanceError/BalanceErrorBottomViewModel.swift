19c360db44c09fc0040e4892960ff88fa0e6356b
Esenbek <esenbekkd@gmail.com>
2026-04-01 15:06:49 +0600

    Minor bugfixes

--- UnstoppableWallet/UnstoppableWallet/Modules/BalanceError/BalanceErrorBottomViewModel.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/BalanceError/BalanceErrorBottomViewModel.swift
@@ -16,6 +16,8 @@ class BalanceErrorBottomViewModel: ObservableObject {
             sourceType = .btc(blockchain: blockchain)
         } else if let blockchain = evmBlockchainManager.blockchain(token: wallet.token) {
             sourceType = .evm(blockchain: blockchain)
+        } else if wallet.token.blockchainType == .tron {
+            sourceType = .evm(blockchain: wallet.token.blockchain)
         } else if wallet.token.blockchainType == .monero {
             sourceType = .monero(blockchain: wallet.token.blockchain)
         } else if wallet.token.blockchainType == .zano {
