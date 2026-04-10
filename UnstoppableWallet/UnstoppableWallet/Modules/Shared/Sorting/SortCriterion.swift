e7c9057000daa55e5c02a77c519613e2cd0b6db5
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2026-04-10 11:14:41 +0600

    Add section comments to SortCriterion criteria sets.

--- UnstoppableWallet/UnstoppableWallet/Modules/Shared/Sorting/SortCriterion.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Shared/Sorting/SortCriterion.swift
@@ -28,23 +28,33 @@ enum SortCriterion {
 }
 
 extension SortCriterion {
+    // Wallet Screen & Send-List Screen. Sort-options
+
     static let walletBalance: [SortCriterion] = [.nonZeroBalanceFirst, .hasPriceFirst, .fiatBalanceDescending, .balanceDescending]
     static let walletName: [SortCriterion] = [.nameAscending]
     static let walletPercentGrowth: [SortCriterion] = [.percentGrowthDescending]
 
+    // Legacy code
     static let coinSelect: [SortCriterion] = [.balanceDescending, .enabled, .marketCapRank, .nameAscending]
     static let coinSelectFiltered: [SortCriterion] = [.balanceDescending, .enabled, .filterRelevance, .marketCapRank, .nameAscending]
 
+    // MultiSwap coin selection
     static let swapEnabled: [SortCriterion] = [.sameBlockchainFirst, .fiatBalanceDescending, .codeAscending, .codeNativeFirst, .blockchainOrder, .badge]
     static let swapSuggested: [SortCriterion] = [.marketCapRank, .codeNativeFirst, .blockchainOrder, .badge]
     static let swapFeatured: [SortCriterion] = [.codeNativeFirst, .blockchainOrder, .badge]
 
+    // MultiSwap filtered by evm-address and text
     static let tokenByBlockchain: [SortCriterion] = [.enabled, .codeNativeFirst, .blockchainOrder, .badge]
     static let tokenFilteredByBlockchain: [SortCriterion] = [.enabled, .filterRelevance, .codeNativeFirst, .blockchainOrder, .badge]
 
+    
+    // Legacy Transactions Screen
     static let transactionToken: [SortCriterion] = [.nameAscending, .codeNativeFirst, .badge]
+
+    // Receive-Blockchains Screen
     static let blockchainList: [SortCriterion] = [.tokenTypeOrder, .blockchainOrder]
 
+    // Receive-List Screen
     static let receiveCoin: [SortCriterion] = [.fiatBalanceDescending, .codeNativeFirst, .blockchainOrder, .nameAscending]
     static let receiveCoinFiltered: [SortCriterion] = [.codeNativeFirst]
 }
