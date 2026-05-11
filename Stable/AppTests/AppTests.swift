b865f9e1576c78130ac1f61cfd32ecac4b2ead1d
EA <ealymbaev@gmail.com>
2026-05-11 18:30:35 +0600

    Add Stable app project to workspace

---
+++ Stable/AppTests/AppTests.swift
@@ -0,0 +1,7 @@
+import Testing
+
+struct AppTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
