529bc52ce2f8f0cf3aefeb38f27ce79a95a5cf43
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2026-06-11 18:58:43 +0600

    Add Zcash as default token when create new wallet.
    * Add test localizable string.

--- packages/WalletCore/Sources/WalletCore/Modules/CreateAccount/CreateAccountViewModel.swift
+++ packages/WalletCore/Sources/WalletCore/Modules/CreateAccount/CreateAccountViewModel.swift
@@ -51,6 +51,7 @@ class CreateAccountViewModel: ObservableObject {
             TokenQuery(blockchainType: .ethereum, tokenType: .native),
             TokenQuery(blockchainType: .monero, tokenType: .native),
             TokenQuery(blockchainType: .tron, tokenType: .native),
+            TokenQuery(blockchainType: .zcash, tokenType: .native),
             TokenQuery(blockchainType: .binanceSmartChain, tokenType: .native),
             TokenQuery(blockchainType: .ethereum, tokenType: .eip20(address: "0xdac17f958d2ee523a2206206994597c13d831ec7")), // USDT
             TokenQuery(blockchainType: .tron, tokenType: .eip20(address: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t")), // USDT
