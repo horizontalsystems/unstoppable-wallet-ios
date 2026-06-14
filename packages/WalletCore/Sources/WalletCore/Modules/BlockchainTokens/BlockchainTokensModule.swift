7a0983cfa378c8a1c9b4caf908d5ce5cf43d26ed
Anton Stavnichiy <anton.stavnichiy@gmail.com>
2026-06-14 19:24:45 +0600

    refactor: remove remaining dead whole files
    Unreachable (zero references anywhere): PaymasterAndData, FeeRateState,
    PaymentRequestAddress, BackupNameView, BlockchainTokensModule, UnlockModule,
    SendEvmModule, SendNftModule, CoinToggleViewModel(+ICoinToggleViewModel).

--- packages/WalletCore/Sources/WalletCore/Modules/BlockchainTokens/BlockchainTokensModule.swift
+++
@@ -1,9 +0,0 @@
-enum BlockchainTokensModule {
-    static func module() -> (BlockchainTokensService, BlockchainTokensView) {
-        let service = BlockchainTokensService()
-        let viewModel = BlockchainTokensViewModel(service: service)
-        let view = BlockchainTokensView(viewModel: viewModel)
-
-        return (service, view)
-    }
-}
